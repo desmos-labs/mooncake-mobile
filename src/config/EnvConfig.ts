0e262dec4a14cc10eee4ec6296031c2c9a0c6139
Kevin Pang <moistbobo@gmail.com>
2022-06-28 15:29:40 +0800

    chore: add graphql (apollo) provider and config

--- src/config/EnvConfig.ts
+++ src/config/EnvConfig.ts
@@ -2,6 +2,11 @@ import Config from 'react-native-config';
 
 const EnvConfig = {
   MMKV_ID: Config.MMKV_ID,
+  GQL_ENDPOINT: {
+    forbole: Config.FORBOLE_GQL,
+    desmos: Config.DESMOS_GQL,
+  },
+  CHAIN_ID: Config.CHAIN_ID,
 };
 
 export default EnvConfig;
