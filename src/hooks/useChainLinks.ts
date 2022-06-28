e0e2cef77677c3b82b93f5a4cb4405b2cc54a913
Kevin Pang <moistbobo@gmail.com>
2022-06-28 15:47:40 +0800

    wip

---
+++ src/hooks/useChainLinks.ts
@@ -0,0 +1,44 @@
+import {ChainLink} from 'types/link';
+
+export default function useChainLinks(address: string) {
+  const userLinks = chainLinks[address];
+
+  useGetChainLinkByAddress({
+    variables: {
+      address,
+    },
+    onCompleted: ({chain_link}) => {
+      const cLinks = chain_link.map(
+        link =>
+          ({
+            chainName: link.chain_config.name,
+            externalAddress: link.external_address,
+            userAddress: link.user_address,
+            creationTime: new Date(`${link.creation_time}Z`),
+          } as ChainLink),
+      );
+      if (userLinks === undefined) {
+        setChainLinks(old => {
+          const newState = {
+            ...old,
+          };
+          newState[address] = cLinks;
+          return newState;
+        });
+      }
+    },
+    onError: () => {
+      if (userLinks === undefined) {
+        setChainLinks(old => {
+          const newState = {
+            ...old,
+          };
+          newState[address] = [];
+          return newState;
+        });
+      }
+    },
+  });
+
+  return userLinks ?? [];
+}
