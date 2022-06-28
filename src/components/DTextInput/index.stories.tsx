1af5834879afa118be8bfafabf5e9ff8a4ea594f
Alessandro Mazzon <alessandro@forbole.com>
2022-06-28 11:40:11 +0200

    feat: DTextInput component

---
+++ src/components/DTextInput/index.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import SbContainer from 'storybook/decorators/SbContainer';
+import DTextInput from './index';
+
+type CompProps = React.ComponentProps<typeof DTextInput>;
+
+const InputError: CompProps = {
+  error: true,
+};
+
+// DTextInput as in Desmos TextInput
+storiesOf('components/DTextInput', module)
+  .addDecorator(getStories => (
+    <SbContainer justifyContent="center" padding={16}>
+      {getStories()}
+    </SbContainer>
+  ))
+  .add('Standard', () => <DTextInput />)
+  .add('Error', () => <DTextInput {...InputError} />);
