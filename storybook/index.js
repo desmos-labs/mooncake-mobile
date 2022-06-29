931232568c0f37cdb7ffe38fc96799c1cb697296
Kevin Pang <moistbobo@gmail.com>
2022-06-30 07:31:10 +0800

    chore(ResultModal): Add story

--- storybook/index.js
+++ storybook/index.js
@@ -4,6 +4,7 @@ import {AppRegistry} from 'react-native';
 import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
 import {withKnobs} from '@storybook/addon-knobs';
 import {loadStories} from './storyLoader';
+import { NavigationContainer } from "@react-navigation/native";
 
 import './rn-addons';
 
