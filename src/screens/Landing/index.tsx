00d6c1f23c5182ce05b8df0f1a27e718464e0b56
Alessandro Mazzon <alessandro@forbole.com>
2022-06-30 17:47:34 +0200

    feat: SettingsOptions (DFP-14) (#7)
    * feat: DView component
    
    * feat: SettingsOptions
    
    * fix: scrollView style
    
    * fix: save atom changes inside the storage

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,17 +1,18 @@
+import DView from 'components/DView';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 import EnvConfig from 'config/EnvConfig';
 
 const Landing = () => {
   return (
-    <View
+    <DView
       style={{
         alignItems: 'center',
         justifyContent: 'center',
       }}>
       <Text>Landing Page</Text>
       <Text>Config: {EnvConfig.MMKV_ID}</Text>
-    </View>
+    </DView>
   );
 };
 
