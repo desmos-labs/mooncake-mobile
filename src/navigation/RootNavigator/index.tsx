2460ec1614fb06bceae9af72e6b5226011697274
Alessandro Mazzon <alessandro@forbole.com>
2022-07-14 10:57:27 +0200

    fix: navigator

--- src/navigation/RootNavigator/index.tsx
+++ src/navigation/RootNavigator/index.tsx
@@ -43,11 +43,10 @@ const RootNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name={ROUTES.USER_PROFILE} component={Profile} />
       <Stack.Screen name={ROUTES.HOME} component={Home} />
-      <Stack.Screen name={ROUTES.ENTER_PASSWORD} component={EnterPassword} />
       <Stack.Screen name={ROUTES.SETTINGS} component={Settings} />
+      <Stack.Screen name={ROUTES.ENTER_PASSWORD} component={EnterPassword} />
       <Stack.Screen name={ROUTES.SETTINGS_PROFILES} component={Profiles} />
       <Stack.Screen name={ROUTES.SETTINGS_COMMUNITY} component={Community} />
-      <Stack.Screen name={ROUTES.HOME} component={Home} />
       <Stack.Screen
         name={ROUTES.LOOKING_FOR_DEVICES}
         component={LookingForDevices}
