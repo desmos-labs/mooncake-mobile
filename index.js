99c18c1837c8d3e8f25b53b1c43c96d3d8cbda7c
Alessandro Mazzon <alessandro@forbole.com>
2022-07-18 15:16:32 +0200

    refactor: index to App

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import './shim';
 import './src/assets/locales/i18n';
 import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
-import StorybookUIRoot from './storybook';
 
-AppRegistry.registerComponent(appName, () => StorybookUIRoot);
+AppRegistry.registerComponent(appName, () => App);
