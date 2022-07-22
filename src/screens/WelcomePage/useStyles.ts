a9371d28230e222dd41af7add72edb871812740c
Alessandro Mazzon <alessandro@forbole.com>
2022-07-22 11:16:30 +0200

    chore: fix style

--- src/screens/WelcomePage/useStyles.ts
+++ src/screens/WelcomePage/useStyles.ts
@@ -19,17 +19,6 @@ const useStyles = makeStyle(theme => ({
     alignItems: 'center',
     marginBottom: theme.spacing.xl,
   },
-  bottomSheet: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-
-    elevation: 24,
-  },
 }));
 
 export default useStyles;
