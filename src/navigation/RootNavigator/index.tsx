958b7dbe75140c1ec93ce1d1aa5c91798c6aa084
Kevin Pang <moistbobo@gmail.com>
2022-07-22 18:30:28 +0800

    feat: SelectDTag screen

--- src/navigation/RootNavigator/index.tsx
+++ src/navigation/RootNavigator/index.tsx
@@ -24,6 +24,7 @@ import MnemonicInput, {
   MnemonicInputParams,
 } from 'screens/MnemonicInput';
 import ShowRecoveryPhrase from 'screens/ShowRecoveryPhrase';
+import SelectDtag from 'screens/SelectDtag';
 
 export type RootNavigatorParamList = {
   [ROUTES.PASSWORD_MANIPULATION]: PasswordManipulationParams;
@@ -42,6 +43,7 @@ export type RootNavigatorParamList = {
   [ROUTES.MNEMONIC_INPUT]: MnemonicInputParams;
   [ROUTES.SETTINGS_REVEAL_SECRET_PHRASE]: undefined;
   [ROUTES.SETTINGS_SHOW_SECRET_PHRASE]: undefined;
+  [ROUTES.SELECT_DTAG]: undefined;
 };
 
 const Stack = createStackNavigator<RootNavigatorParamList>();
@@ -53,6 +55,8 @@ const RootNavigator = () => {
 
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name={ROUTES.SELECT_DTAG} component={SelectDtag} />
+
       <Stack.Screen
         initialParams={{
           mode: MNEMONIC_INPUT_MODE.IMPORT_RECOVERY_PHRASE,
--- src/screens/SelectDtag/components/ProfileItem/index.tsx
+++ src/screens/SelectDtag/components/ProfileItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image, ImageSourcePropType, TouchableOpacity, View} from 'react-native';
 import Typography from 'components/Typography';
 import DropShadowWrapper from 'components/DropShadowWrapper';
+import {useTranslation} from 'react-i18next';
 import useStyles from './useStyles';
 
 type Props = {
@@ -17,13 +18,22 @@ type Props = {
 const ProfileItem = ({nickname, dtag, handlePress, avatar}: Props) => {
   const styles = useStyles();
 
+  const {t} = useTranslation('selectDtag');
+
   return (
     <DropShadowWrapper customColor="rgba(16, 24, 40,0.05)" disableInnerWrapper>
       <TouchableOpacity onPress={handlePress} style={styles.container}>
-        <Image source={avatar} style={styles.avatar} />
+        {/* this is ok as we can be certain that all avatars will be from a uri */}
+        {/* source */}
+        {/* @ts-ignore */}
+        {avatar && avatar?.uri ? (
+          <Image source={avatar} style={styles.avatar} />
+        ) : (
+          <View style={[styles.avatar, {backgroundColor: 'gray'}]} />
+        )}
 
         <View>
-          <Typography.H5>{nickname}</Typography.H5>
+          <Typography.H5>{nickname || t('noNickname')}</Typography.H5>
 
           <Typography.Body6>@{dtag}</Typography.Body6>
         </View>
