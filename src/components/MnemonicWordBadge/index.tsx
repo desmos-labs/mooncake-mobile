b5ff3ae893ceb71f0235860e5ce6984fef94e19d
Alessandro Mazzon <alessandro@forbole.com>
2022-07-22 10:10:30 +0200

    fix: typography

--- src/components/MnemonicWordBadge/index.tsx
+++ src/components/MnemonicWordBadge/index.tsx
@@ -39,7 +39,7 @@ const MnemonicWordBadge: React.FC<Props> = props => {
             }
           : undefined
       }>
-      <Typography.Button2>{value}</Typography.Button2>
+      <Typography.Body6>{value}</Typography.Body6>
       <Text style={styles.index}>{index}</Text>
     </TouchableOpacity>
   );
