c3324e6eaad6dd975105a782125f0df6b6fa6c6f
Kevin Pang <moistbobo@gmail.com>
2022-07-27 11:30:39 +0800

    feat: Welcome back screen (#41)

---
+++ src/screens/WelcomeBack/useStyles.ts
@@ -0,0 +1,45 @@
+import {makeStyle} from 'config/theme';
+import {verticalScale} from 'react-native-size-matters';
+
+const useStyles = makeStyle(theme => ({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    padding: theme.spacing.m,
+    paddingTop: verticalScale(56),
+  },
+  dummyAvatar: {
+    width: 129,
+    height: 129,
+    resizeMode: 'contain',
+    borderRadius: 64.5,
+  },
+  headerStyle: {
+    marginTop: 46,
+    fontSize: 32,
+    lineHeight: 40,
+    textAlign: 'center',
+    color: theme.colors.white,
+  },
+  descriptionStyle: {
+    color: theme.colors.white,
+    marginBottom: 50,
+  },
+  connectLedgerButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  connectLedgerImage: {
+    width: 24,
+    height: 20,
+    resizeMode: 'contain',
+    marginRight: theme.spacing.s,
+  },
+  forgotPwGroup: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+}));
+
+export default useStyles;
