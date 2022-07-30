73297c0da9aa7a0960869b5bfe4101b8438faf09
Kevin Pang <moistbobo@gmail.com>
2022-07-30 17:33:17 +0800

    wip: Setup BroadcastTx page

--- src/screens/BroadcastTx/index.tsx
+++ src/screens/BroadcastTx/index.tsx
@@ -6,13 +6,61 @@ import {RootNavigatorParamList} from 'navigation/RootNavigator';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {View} from 'react-native';
+import {EncodeObject} from '@cosmjs/proto-signing';
+import ROUTES from 'navigation/routes';
+import useBroadcastMessages from 'hooks/useBroadcastMessages';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {computeTxFees, messagesGas} from 'lib/desmos/fees';
+import LocalWallet from 'lib/LocalWallet';
 import useStyles from './useStyles';
 
-declare type Props = StackScreenProps<RootNavigatorParamList>;
+export type BroadcastTxParams = {
+  messages: EncodeObject[];
 
-const BroadcastTx: React.FC<Props> = () => {
+  serializedWallet: string;
+
+  granter?: string;
+};
+
+type NavProps = StackScreenProps<RootNavigatorParamList, ROUTES.BROADCAST_TX>;
+
+const BroadcastTx: React.FC = () => {
   const {t} = useTranslation('accountCreation');
   const styles = useStyles();
+  const {push} = useNavigation<NavProps['navigation']>();
+  const {params} = useRoute<NavProps['route']>();
+  const [error, setError] = React.useState('');
+  const broadcastMessages = useBroadcastMessages();
+
+  const broadcastTx = React.useCallback(async () => {
+    const {messages, granter, serializedWallet} = params;
+
+    const wallet = await LocalWallet.deserialize(serializedWallet);
+
+    const gas = messagesGas(messages);
+    // hardcoded denom for now
+    const txFee = computeTxFees(gas, 'udaric').average;
+
+    try {
+      await broadcastMessages(wallet, messages, txFee, '', granter);
+
+      // success
+
+      push(ROUTES.RESULT_MODAL, {
+        title: t('resultModal:congrats'),
+        subtitle: t('resultModal:profileCreated'),
+        primaryButtonLabel: t('resultModal:enterApp'),
+        onPressPrimary: () => {},
+      });
+    } catch (err: any) {
+      setError(err.message);
+      console.log(error);
+    }
+  }, [params]);
+
+  React.useEffect(() => {
+    broadcastTx();
+  }, []);
 
   return (
     <DView>
