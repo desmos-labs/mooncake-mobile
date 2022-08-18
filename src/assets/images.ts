a0ad3fcb0a65decdafb37ab9a555078432592c2b
Kevin Pang <moistbobo@gmail.com>
2022-08-18 10:07:31 +0800

    wip: add post background images

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -150,7 +150,22 @@ export const textPostIcon = require('./images/textPost.png');
 
 export const whiteCross = require('./images/whiteCross.png');
 
-//Socials
+// array of images
+export const postBG = [
+  require('./images/postBG/postBG1.png'),
+  require('./images/postBG/postBG2.png'),
+  require('./images/postBG/postBG3.png'),
+  require('./images/postBG/postBG4.png'),
+  require('./images/postBG/postBG5.png'),
+  require('./images/postBG/postBG6.png'),
+  require('./images/postBG/postBG7.png'),
+  require('./images/postBG/postBG8.png'),
+  require('./images/postBG/postBG9.png'),
+  require('./images/postBG/postBG10.png'),
+  require('./images/postBG/postBG11.png'),
+]
+
+// Socials
 
 export const discordIcon = require('./images/discord.png');
 
