2abc61847c0ccbb0971669772f4afda8c8788674
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-29 20:40:26 +0800

    DFP-298 add gql script

--- src/services/graphql/queries/GetFollowingAndFollowers.ts
+++ src/services/graphql/queries/GetFollowingAndFollowers.ts
@@ -44,3 +44,37 @@ export const GetFollowingForAddress = gql`
     }
   }
 `;
+
+/* A GraphQL query. */
+export const GetFollowersForAddress = gql`
+  query GetFollowersForAddress(
+    $subspaceID: bigint!
+    $userAddress: String!
+    $limit: Int!
+    $offset: Int!
+  ) @api(name: desmos) {
+    user_relationship(
+      limit: $limit
+      offset: $offset
+      where: {
+        subspace_id: {_eq: $subspaceID}
+        counterparty_address: {_eq: $userAddress}
+      }
+      order_by: {creator: {creation_time: asc}}
+    ) {
+      _: creator {
+        dtag
+        nickname
+        profile_pic
+        address
+      }
+    }
+    user_relationship_aggregate(
+      where: {counterparty_address: {_eq: $userAddress}}
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
