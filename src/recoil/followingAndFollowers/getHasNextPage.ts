73c8e16d9c7cb423f9b54bb7355f2ad4d7cee8d6
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-29 23:05:19 +0800

    docs: explain how getHasNextPage works

--- src/recoil/followingAndFollowers/getHasNextPage.ts
+++ src/recoil/followingAndFollowers/getHasNextPage.ts
@@ -1,15 +1,26 @@
 import {Loadable} from 'recoil';
 import {PaginatedData, PAGINATION_LIMIT} from '.';
 
+/**
+ * Return true if there are more pages to load, and we want to load them now.
+ * @param {number} currentPageLimit - The current page limit.
+ * @param query - (page: number) => Loadable<PaginatedData<T>>
+ * @returns A boolean value.
+ */
 function getHasNextPage<T>(
   currentPageLimit: number,
   query: (page: number) => Loadable<PaginatedData<T>>,
 ) {
   for (let page = 1; page <= currentPageLimit; page++) {
     const loadable = query(page);
+
+    /* If the loadable is not in the `hasValue` state, then we know that the previous API is still running. */
     if (loadable.state !== 'hasValue') {
       return false;
     }
+
+    /* If the length of the data is less than the limit, then we know that we don't have any more data
+    to load. */
     if (loadable.getValue().data.length < PAGINATION_LIMIT) {
       return false;
     }
