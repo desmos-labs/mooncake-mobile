8e42637b2b710e019314c9d9689c57386d8f0005
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-29 23:27:57 +0800

    docs: descript the properties

--- src/screens/FollowingAndFollowers/FollowersParams.tsx
+++ src/screens/FollowingAndFollowers/FollowersParams.tsx
@@ -1,7 +1,15 @@
+/**
+ * @property {number} initSubspaceID - The subspace ID of the app.
+ * @property {string} initUserAddress - The address of the user we want to display.
+ * @property {number} initialTabIndex - The index of the tab that should be selected when the component
+ * is first rendered.
+ * @property {string} username - The username of the user whose followers you want to see.
+ */
 type FollowersParams = {
   initSubspaceID: number;
   initUserAddress: string;
   initialTabIndex: number;
   username: string;
 };
+
 export default FollowersParams;
