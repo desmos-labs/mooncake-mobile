402c1c7312589fcefccb29f150c95ca438319db6
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-29 23:30:50 +0800

    docs: explain how page liimt works

--- src/recoil/followingAndFollowers/pageLimitOfFollowersState.ts
+++ src/recoil/followingAndFollowers/pageLimitOfFollowersState.ts
@@ -1,7 +1,6 @@
 import {atom} from 'recoil';
 
-/* It's creating a selector that fetches the following accounts for a given page. */
-
+/* the page limit will be increase when we scroll to the bottom of the FlatList and there is more pages to load from server. */
 const pageLimitOfFollowersState = atom<number>({
   key: 'pageLimitOfFollowers',
   default: 1,
