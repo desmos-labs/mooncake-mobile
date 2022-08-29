8d2537f85d535dfa7a62cac6c3eddb8b432555ed
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-29 20:39:25 +0800

    DFP-297 add gql script

---
+++ src/services/graphql/queries/GetFollowingAndFollowers.ts
@@ -0,0 +1,46 @@
+import {gql} from '@apollo/client';
+
+export type QueueData = {
+  user_relationship: Array<{
+    _: FollowersData;
+  }>;
+  user_relationship_aggregate: {
+    aggregate: {
+      count: number;
+    };
+  };
+};
+
+/* A GraphQL query. */
+export const GetFollowingForAddress = gql`
+  query GetFollowingForAddress(
+    $subspaceID: bigint!
+    $userAddress: String!
+    $limit: Int!
+    $offset: Int!
+  ) @api(name: desmos) {
+    user_relationship(
+      limit: $limit
+      offset: $offset
+      where: {
+        subspace_id: {_eq: $subspaceID}
+        creator_address: {_eq: $userAddress}
+      }
+      order_by: {counterparty: {creation_time: asc}}
+    ) {
+      _: counterparty {
+        dtag
+        nickname
+        profile_pic
+        address
+      }
+    }
+    user_relationship_aggregate(
+      where: {counterparty_address: {_eq: $userAddress}}
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
