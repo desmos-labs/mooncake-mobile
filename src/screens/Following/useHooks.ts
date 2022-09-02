ad92e07ff3beba2eedb6fe375a9beb66dfcc09cf
teamchong <25894545+teamchong@users.noreply.github.com>
2022-09-03 01:46:42 +0800

    use another hook for testing

--- src/screens/Following/useHooks.ts
+++ src/screens/Following/useHooks.ts
@@ -6,87 +6,6 @@ import {useCallback, useEffect} from 'react';
 import {useSetRecoilState} from 'recoil';
 import numOfFollowerState from '@recoil/numOfFollowerState';
 
-// // START debug
-// import {ApolloError} from '@apollo/client';
-// import {QueueData} from 'services/graphql/queries/GetPaginatedFollowing';
-// import {
-//   MutableRefObject,
-//   useCallback,
-//   useEffect,
-//   useRef,
-//   useState,
-// } from 'react';
-// import {useSetRecoilState} from 'recoil';
-// import numOfFollowerState from '@recoil/numOfFollowerState';
-
-// const MAX_MOCK_FOLLOWERS = 10000000;
-// function mockGaginatedFollowers(
-//   ref: MutableRefObject<QueueData['paginatedFollowers']>,
-//   initialOffset: number,
-// ) {
-//   const paginatedFollowers = ref.current;
-//   let offset = initialOffset;
-//   for (let i = 0; i < 20; i++) {
-//     if (offset >= MAX_MOCK_FOLLOWERS) break;
-//     const i1 = {
-//       _: {
-//         dtag: `GoFind.${offset}`,
-//         address: 'desmos1p7ce9sydhm7dsugl890g9unmp99nmq0gz9f4vx',
-//         nickname: `GoFind.${offset}`,
-//         profile_pic: `https://picsum.photos/40?.${offset}`,
-//       },
-//     };
-//     if (offset in paginatedFollowers) paginatedFollowers[offset] = i1;
-//     else paginatedFollowers.push(i1);
-//     offset++;
-//     const i2 = {
-//       _: {
-//         dtag: `Masternode24.${offset}`,
-//         address: 'desmos1h5f3dywec65v9qulxkmcv3e6yujyh3zm39lr4r',
-//         nickname: `Masternode24.de.${offset}`,
-//         profile_pic: `https://picsum.photos/40?.${offset}`,
-//       },
-//     };
-//     if (offset in paginatedFollowers) paginatedFollowers[offset] = i2;
-//     else paginatedFollowers.push(i2);
-//     offset++;
-//     const i3 = {
-//       _: {
-//         dtag: `dima_student2.${offset}`,
-//         address: 'desmos1ys42amj53hka8mx4h2nvz4hxf82v9rwvn6xuxh',
-//         nickname: `dima_student2#2856.${offset}`,
-//         profile_pic: `https://picsum.photos/40?.${offset}`,
-//       },
-//     };
-//     if (offset in paginatedFollowers) paginatedFollowers[offset] = i3;
-//     else paginatedFollowers.push(i3);
-//     const i4 = {
-//       _: {
-//         dtag: `!Masternode24.${offset}`,
-//         address: 'desmos1h5f3dywec65v9qulxkmcv3e6yujyh3zm39lr4r',
-//         nickname: `!Masternode24.de.${offset}`,
-//         profile_pic: `https://picsum.photos/40?.${offset}`,
-//       },
-//     };
-//     if (offset in paginatedFollowers) paginatedFollowers[offset] = i4;
-//     else paginatedFollowers.push(i4);
-//     offset++;
-//     const i5 = {
-//       _: {
-//         dtag: `!dima_student2.${offset}`,
-//         address: 'desmos1ys42amj53hka8mx4h2nvz4hxf82v9rwvn6xuxh',
-//         nickname: `!dima_student2#2856.${offset}`,
-//         profile_pic: '',
-//       },
-//     };
-//     if (offset in paginatedFollowers) paginatedFollowers[offset] = i5;
-//     else paginatedFollowers.push(i5);
-//     offset++;
-//   }
-//   return paginatedFollowers;
-// }
-// // END debug
-
 /* It's setting the limit of the number of items to be fetched. */
 export const ITEMS_PER_FETCH = 100;
 
@@ -122,51 +41,6 @@ const useHooks = (subspaceID: number, userAddress: string) => {
       },
     },
   );
-
-  // // START debug
-  // const paginatedFollowers = useRef<QueueData['paginatedFollowers']>([]);
-  // const [data, setData] = useState<QueueData>();
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState<ApolloError>();
-  // function fetchMore(params: {variables: {offset: number}}) {
-  //   console.log('fetchMore', params);
-  //   return fetch(params.variables.offset);
-  // }
-  // function refetch(params: {offset: number}) {
-  //   console.log('refetch', params);
-  //   return fetch(params.offset);
-  // }
-  // function fetch(offset: number) {
-  //   console.log('fetch', offset);
-  //   setLoading(true);
-  //   setError(undefined);
-  //   setTimeout(
-  //     () => {
-  //       console.log('fetching', offset);
-  //       if (Math.random() > 1) {
-  //         setLoading(false);
-  //         setError(new ApolloError({errorMessage: 'random error'}));
-  //         return;
-  //       }
-  //       setLoading(false);
-  //       setError(undefined);
-  //       setData({
-  //         paginatedFollowers: mockGaginatedFollowers(
-  //           paginatedFollowers,
-  //           offset,
-  //         ),
-  //         user_relationship_aggregate: {
-  //           aggregate: {
-  //             count: MAX_MOCK_FOLLOWERS,
-  //           },
-  //         },
-  //       });
-  //     },
-  //     Math.random() > 1 ? 60000 : 1000,
-  //   );
-  // }
-  // useEffect(() => fetch(0), []);
-  // // END debug
 
   const setNumOfFollowers = useSetRecoilState(
     numOfFollowerState({type: 'following', subspaceID, userAddress}),
