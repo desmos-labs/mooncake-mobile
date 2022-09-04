3ccc97f87b77769fbcf13fd44802dfeef9eb48f1
teamchong <25894545+teamchong@users.noreply.github.com>
2022-09-04 18:53:29 +0800

    refactor: use paddingVertical

--- src/screens/Following/components/Loading/useStyles.ts
+++ src/screens/Following/components/Loading/useStyles.ts
@@ -3,8 +3,7 @@ import {makeStyle} from 'config/theme';
 const useStyles = makeStyle(() => ({
   loadingContainer: {
     flexGrow: 1,
-    paddingTop: 50,
-    paddingBottom: 50,
+    paddingVertical: 50,
     justifyContent: 'center',
     alignContent: 'center',
   },
