7896be39fba77bf4e984414a28c5f5808d731e82
Alessandro Mazzon <alessandro@forbole.com>
2022-09-06 04:28:06 +0200

    feat(DFP-428): Profile visualization (#82)
    * feat: DFP-428
    
    * fix: selected address fix
    
    * fix: PR fix and query refactor
    
    * fix: PR fix
    
    * fix: super bad if
    
    * fix: removed fragment

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -70,7 +70,7 @@ const Home = () => {
     onCarouselProgressChange,
     onPostChanged,
     postData,
-  } = useHooks();
+  } = useHooks(activeAddress!);
 
   const {profileData} = useActiveAccount();
 
@@ -82,7 +82,7 @@ const Home = () => {
       return (
         <PostCard
           postData={info.item}
-          onPressAuthor={() => handlePressAuthor('')}
+          onPressAuthor={() => handlePressAuthor(info.item.author_address)}
           onPressDetails={() =>
             handlePressDetails(info.item.id, info.item.subspace_id)
           }
@@ -138,7 +138,7 @@ const Home = () => {
   }, []);
 
   const handlePressProfile = React.useCallback(() => {
-    navigate(ROUTES.USER_PROFILE);
+    navigate(ROUTES.USER_PROFILE, {});
   }, []);
 
   const profilePic = _.get(profileData, 'profile_pic');
