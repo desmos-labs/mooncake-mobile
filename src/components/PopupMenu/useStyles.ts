7ed3df9fe01ef0df8e7a513c00d22b7614cef674
Kevin Pang <moistbobo@gmail.com>
2022-09-15 18:11:20 +0800

    fix(DFP-472): [Post Details] Share & Report UI Updates (#103)
    * fix(DFP-472): Popup share/report menu background color
    
    * refactor: Adjust popup menu popup location
    
    * refactor: adjust popup menu icon size, alignment

--- src/components/PopupMenu/useStyles.ts
+++ src/components/PopupMenu/useStyles.ts
@@ -1,15 +1,19 @@
 import {makeStyle} from 'config/theme';
 
 const useStyles = makeStyle(theme => ({
+  container: {
+    backgroundColor: theme.colors.white,
+  },
   icon: {
     marginRight: theme.spacing.s,
-    height: 32,
-    width: 32,
+    height: 24,
+    width: 24,
+    resizeMode: 'contain',
   },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingHorizontal: theme.spacing.m,
     marginRight: theme.spacing.m,
     paddingVertical: theme.spacing.s,
