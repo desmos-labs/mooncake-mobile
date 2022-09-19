24f4695cc404946b790278e38b4ecd89369e6d5e
Alessandro Mazzon <alessandro@forbole.com>
2022-09-19 09:18:59 +0200

    fix: pr review

--- src/screens/Profile/components/ChainsCountersBar/index.tsx
+++ src/screens/Profile/components/ChainsCountersBar/index.tsx
@@ -22,13 +22,6 @@ const ChainsCountersBar = ({
 }: Props) => {
   const styles = useStyles();
   const {t} = useTranslation('profile');
-  // TODO i dont like this but i had not found any better idea
-  const calculatedWidth =
-    connectedChainsImages.length === 1
-      ? 30
-      : connectedChainsImages.length === 2
-      ? 50
-      : 70;
 
   return loading ? (
     <ActivityIndicator />
@@ -36,16 +29,18 @@ const ChainsCountersBar = ({
     <View style={styles.container}>
       <TouchableOpacity onPress={handlePressCounters} style={styles.button}>
         {connectedChainsImages[0] && (
-          <View style={{width: calculatedWidth, height: 30}}>
-            {connectedChainsImages[0] && (
-              <Image source={connectedChainsImages[0]} style={styles.icon1} />
-            )}
-            {connectedChainsImages[1] && (
-              <Image source={connectedChainsImages[1]} style={styles.icon2} />
-            )}
-            {connectedChainsImages[2] && (
-              <Image source={connectedChainsImages[2]} style={styles.icon3} />
-            )}
+          <View
+            style={{
+              flexDirection: 'row',
+              marginRight: -6 * connectedChainsImages.length,
+            }}>
+            {connectedChainsImages.map((x, idx) => (
+              <Image
+                key={x.toString()}
+                source={x}
+                style={[styles.iconStyle, {left: -10 * idx}]}
+              />
+            ))}
           </View>
         )}
         <Typography.Button2 style={styles.text}>
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -259,12 +259,12 @@ const Profile = () => {
                     </Typography.Button2>
                   </Button>
                 </View>
-                {chainLinks.length > 0 && (
+                {chainLinks.length === 0 && (
                   <View style={{marginTop: 12}}>
                     <ChainsCountersBar
                       loading={false}
                       connectedChainsCounter={chainLinks.length}
-                      connectedAppsCounter={0}
+                      connectedAppsCounter={6}
                       connectedChainsImages={[
                         stargazeIcon,
                         cosmosIcon,
