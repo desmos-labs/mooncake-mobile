5a09ce9008ce4abbf1dfb54bc76834d4a3469a4e
Kevin Pang <moistbobo@gmail.com>
2022-09-19 14:45:28 +0800

    chore: remove console.log

--- src/recoil/posts.ts
+++ src/recoil/posts.ts
@@ -44,7 +44,6 @@ export const useGetPosts = () => {
       limit: POSTS_PER_FETCH,
       subspaceID,
     }).then(() => {
-      console.log('finished fetching more posts');
       newOffset.current += POSTS_PER_FETCH;
     });
   }, [newOffset.current]);
