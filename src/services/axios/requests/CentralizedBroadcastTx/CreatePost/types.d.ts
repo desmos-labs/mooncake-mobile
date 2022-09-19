f810e640874fdee6269bc634389c411eb088f1f3
Kevin Pang <moistbobo@gmail.com>
2022-09-19 17:53:38 +0800

    chore: add TODO

--- src/services/axios/requests/CentralizedBroadcastTx/CreatePost/types.d.ts
+++ src/services/axios/requests/CentralizedBroadcastTx/CreatePost/types.d.ts
@@ -2,6 +2,7 @@ import {PostReference} from '@desmoslabs/desmjs-types/desmos/posts/v2/models';
 
 export {};
 
+// TODO: use types from desmjs instead
 declare global {
   type CreatePostParams = {
     text: string;
