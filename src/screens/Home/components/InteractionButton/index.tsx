48806710db6b1ae265c205716beff72c78042074
Alessandro Mazzon <alessandro@forbole.com>
2022-09-21 12:06:04 +0200

    fix: added some shadows to home buttons (design change)

--- src/screens/Home/components/InteractionButton/index.tsx
+++ src/screens/Home/components/InteractionButton/index.tsx
@@ -2,6 +2,7 @@ import Typography from 'components/Typography';
 import {formatNumShorthand} from 'lib/FormatUtils';
 import React from 'react';
 import {Image, ImageSourcePropType, TouchableOpacity} from 'react-native';
+import {Shadow} from 'react-native-shadow-2';
 import useStyles from './useStyles';
 
 type Props = {
@@ -25,12 +26,17 @@ const InteractionButton = ({onPress, interactionCount, icon}: Props) => {
   const styles = useStyles();
 
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Image source={icon} style={styles.icon} />
-      <Typography.Subtitle3 style={styles.countText}>
-        {formatNumShorthand(interactionCount)}
-      </Typography.Subtitle3>
-    </TouchableOpacity>
+    <Shadow
+      finalColor="rgba(133, 133, 133, 0)"
+      startColor="rgba(133, 133, 133, 0.13)"
+      distance={12}>
+      <TouchableOpacity style={styles.container} onPress={onPress}>
+        <Image source={icon} style={styles.icon} />
+        <Typography.Subtitle3 style={styles.countText}>
+          {formatNumShorthand(interactionCount)}
+        </Typography.Subtitle3>
+      </TouchableOpacity>
+    </Shadow>
   );
 };
 
