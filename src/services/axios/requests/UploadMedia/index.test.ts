4871c21c9459008f4db7c62cb5e6d4a75890ce9d
Kevin Pang <moistbobo@gmail.com>
2022-09-22 14:11:43 +0800

    test(UploadMedia): Update test

--- src/services/axios/requests/UploadMedia/index.test.ts
+++ src/services/axios/requests/UploadMedia/index.test.ts
@@ -21,20 +21,4 @@ describe('services: axios/UploadMedia', () => {
 
     expect(response).toEqual(MOCKED_DATA);
   });
-
-  it('Calls optional error handler if an error occurs', async () => {
-    const onErrorMock = jest.fn();
-
-    (axiosInstance.post as jest.Mock).mockRejectedValueOnce({
-      status: 400,
-      data: MOCKED_DATA,
-    });
-
-    await UploadMedia({
-      mediaFile: {uri: '123', type: '123', fileName: '123'},
-      onError: onErrorMock,
-    });
-
-    expect(onErrorMock).toHaveBeenCalledTimes(1);
-  });
 });
