6d0a691f9bcf5a232ecb46fb2e1f8c56ce1b5858
Kevin Pang <moistbobo@gmail.com>
2022-09-22 17:02:32 +0800

    fix(EnterCommentBottomBar): android height adjustment

--- src/components/EnterCommentBottomBar/index.tsx
+++ src/components/EnterCommentBottomBar/index.tsx
@@ -154,11 +154,10 @@ const EnterCommentBottomBar: React.FC<Props> = ({
                 maxLength={EnvConfig.MAX_COMMENT_LENGTH}
                 value={comment}
                 onChangeText={text => setComment(text)}
-                multiline={true}
+                multiline
                 style={styles.textInput}
                 placeholderTextColor={theme.colors.grey02}
                 placeholder={t('write a comment')}
-                textAlignVertical="center"
               />
 
               <View
@@ -170,6 +169,10 @@ const EnterCommentBottomBar: React.FC<Props> = ({
                 <ImageButton
                   style={{
                     opacity: keyboardShow ? 1 : 0,
+                    bottom: Platform.select({
+                      ios: 0,
+                      android: 8,
+                    }),
                   }}
                   image={expandCommentIcon}
                   onPress={onIconPress}
