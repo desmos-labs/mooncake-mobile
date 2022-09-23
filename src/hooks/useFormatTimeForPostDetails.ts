e44fbd0933abfbb3d992efd865eb782b8fce6856
Kevin Pang <moistbobo@gmail.com>
2022-09-23 16:34:32 +0800

    reformat: useFormatTimeForPostDetails to use new useFormatDateToTZ

--- src/hooks/useFormatTimeForPostDetails.ts
+++ src/hooks/useFormatTimeForPostDetails.ts
@@ -1,36 +1,20 @@
 import React from 'react';
-import {useRecoilState} from 'recoil';
-import appSettingsState from '@recoil/settings';
 import {differenceInYears, parseISO} from 'date-fns';
-import {formatInTimeZone} from 'date-fns-tz';
+import useFormatDateToTZ from 'hooks/formatting/useFormatDateToTZ';
 
 /**
  * A hook that formats an ISO 8601 timestamp into the user's local timezone
  */
 const useFormatTimeForPostDetails = (timeToFormat: string) => {
-  const [settings] = useRecoilState(appSettingsState);
-
-  return React.useMemo(() => {
-    // sanity check, but this should never happen
-    if (!timeToFormat) return '';
-    // append Z so date-fns can process offsets properly
-    // for reference, the time is stored as UTC +0
+  const shouldRenderDetailedTime = React.useMemo(() => {
     const parsedTime = parseISO(`${timeToFormat}Z`);
-    if (differenceInYears(new Date(parsedTime), Date.now()) === 0) {
-      return formatInTimeZone(
-        parsedTime,
-        settings.currentTimezone,
-        // eg. 20 Apr, 04:20
-        'dd MMM, HH:mm',
-      );
-    }
-    return formatInTimeZone(
-      parsedTime,
-      settings.currentTimezone,
-      // eg. Sat Apr 20 2019
-      'ccc MMM dd yyyy',
-    );
-  }, [settings, timeToFormat]);
+    return differenceInYears(new Date(parsedTime), Date.now()) === 0;
+  }, [timeToFormat]);
+
+  return useFormatDateToTZ(
+    timeToFormat,
+    shouldRenderDetailedTime ? 'dd MMM, HH:mm' : 'ccc MMM dd yyyy',
+  );
 };
 
 export default useFormatTimeForPostDetails;
