f88d7053ec869c79ac812c9f7b8061e33f2884ba
Kevin Pang <moistbobo@gmail.com>
2022-09-24 23:50:36 +0800

    refactor: split checkGrants into separate utils.ts

--- src/hooks/authGrants/useCheckAndUpdateGrants/index.ts
+++ src/hooks/authGrants/useCheckAndUpdateGrants/index.ts
@@ -1,48 +1,8 @@
 import React from 'react';
 import {GrantEnums} from 'lib/desmos/msgtypes';
-import {differenceInMilliseconds} from 'date-fns';
-import GetActiveGrants from 'services/axios/requests/GetActiveGrants';
 import {useNavigation} from '@react-navigation/native';
 import ROUTES from 'navigation/routes';
-
-// adapted from useCheckGrants
-/**
- * Check if a given account by address has grantsToCheck grants
- * @param {GrantEnums[]} grantsToCheck The array of grants to check.
- * @param {string} address The address of the account to check grants for.
- * @return {Promise<GrantEnums[]>} A promise that resolves to an array of GrantEnums that the account does not have
- *                                 or have expired.
- */
-const checkGrants = async (
-  grantsToCheck: GrantEnums[],
-  address: string,
-): Promise<GrantEnums[]> => {
-  const grantsResponse = await GetActiveGrants({address});
-
-  const grants: {
-    [index: string]: {msg_type: GrantEnums; expiration: string};
-  } = grantsResponse.grants.reduce((acc, cur) => {
-    return {
-      ...acc,
-      [cur.msg_type]: cur,
-    };
-  }, {});
-
-  return grantsToCheck.filter(x => {
-    if (grants[x]) {
-      const differenceFromNow = differenceInMilliseconds(
-        Date.now(),
-        new Date(grants[x].expiration),
-      );
-
-      if (differenceFromNow >= 0) {
-        console.log('grant', grants[x].msg_type, 'is expired');
-        return grants[x];
-      }
-    }
-    return !grants[x];
-  });
-};
+import {checkGrants} from './utils';
 
 const useCheckAndUpdateGrants = () => {
   const {navigate, pop} = useNavigation<any>();
