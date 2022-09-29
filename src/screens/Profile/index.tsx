b1bb36b7e823d1b8c41c1f3bf04cb62e57d7c6e6
Alessandro Mazzon <alessandro@forbole.com>
2022-09-29 15:05:34 +0200

    fix: chains hide if no chainlink

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -259,7 +259,7 @@ const Profile = () => {
                     </Typography.Button2>
                   </Button>
                 </View>
-                {chainLinks.length === 0 && (
+                {chainLinks.length !== 0 && (
                   <View style={{marginTop: 12}}>
                     <ChainsCountersBar
                       loading={false}
