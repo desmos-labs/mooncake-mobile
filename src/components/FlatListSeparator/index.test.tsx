724d22e782881c33c1c75a3201eb556d9c682368
Kevin Pang <moistbobo@gmail.com>
2022-10-03 14:49:19 +0800

    test(FlatListSeparator): Add snapshot test

---
+++ src/components/FlatListSeparator/index.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'jest/utils/CustomRender';
+import FlatListSeparator from 'components/FlatListSeparator/index';
+
+describe('component: FlatListSeparator', () => {
+  it('renders', () => {
+    const tree = render(<FlatListSeparator />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
