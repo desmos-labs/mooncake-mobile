be0664af34f0c837e757e8ab8f3fca4c7af33039
Kevin Pang <moistbobo@gmail.com>
2022-10-03 15:08:33 +0800

    test(LoadingOverlay): Add snapshot test

---
+++ src/components/LoadingOverlay/index.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'jest/utils/CustomRender';
+import LoadingOverlay from 'components/LoadingOverlay/index';
+
+describe('component: LoadingOverlay', () => {
+  it('renders', () => {
+    const tree = render(<LoadingOverlay isVisible />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('does not render if isVisible is false', () => {
+    const tree = render(<LoadingOverlay isVisible={false} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
