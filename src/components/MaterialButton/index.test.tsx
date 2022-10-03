690c2ab1ac9203ea3a35778e382eedf5ac1ef52d
Kevin Pang <moistbobo@gmail.com>
2022-10-03 15:44:05 +0800

    test(MaterialButton): Add snapshot tests

---
+++ src/components/MaterialButton/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {render} from 'jest/utils/CustomRender';
+import MaterialButton from 'components/MaterialButton/index';
+
+describe('component: MaterialButton', () => {
+  it('renders', () => {
+    const tree = render(<MaterialButton />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders mode: text', () => {
+    const tree = render(<MaterialButton mode="text" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders mode: gradient', () => {
+    const tree = render(<MaterialButton mode="gradient" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders mode: gradientFilled', () => {
+    const tree = render(<MaterialButton mode="gradientFilled" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders mode: backgroundComponent', () => {
+    const tree = render(<MaterialButton mode="backgroundComponent" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
