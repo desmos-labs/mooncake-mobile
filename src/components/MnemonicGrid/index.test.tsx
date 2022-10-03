45c812531d8d0d1155432c6921223f738d323da6
Kevin Pang <moistbobo@gmail.com>
2022-10-03 15:48:41 +0800

    test(MnemonicGrid): Add tests

---
+++ src/components/MnemonicGrid/index.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'jest/utils/CustomRender';
+import MnemonicGrid from 'components/MnemonicGrid/index';
+
+describe('component: MnemonicGrid', () => {
+  it('renders', () => {
+    const tree = render(
+      <MnemonicGrid mnemonic="drill rather remain skill online food pool prevent penalty cry cable title fragile flip remind famous dismiss island erase crash column service minute mushroom" />,
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
