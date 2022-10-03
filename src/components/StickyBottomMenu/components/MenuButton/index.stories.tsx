dfe61ed673b66155c3d6c07330d23b7a6ecf74d1
Kevin Pang <moistbobo@gmail.com>
2022-10-04 03:06:57 +0800

    chore(StickyBottomMenu): Update stories, mark as unused

---
+++ src/components/StickyBottomMenu/components/MenuButton/index.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import SbContainer from 'storybook/decorators/SbContainer';
+import {action} from '@storybook/addon-actions';
+import {commentIcon} from 'assets/images';
+import MenuButton from './index';
+
+type CompProps = React.ComponentProps<typeof MenuButton>;
+
+const StandardProps: CompProps = {
+  onPress: action('onPress'),
+
+  interactionCount: 100,
+
+  icon: commentIcon,
+};
+
+storiesOf('components/MenuButton', module)
+  .addDecorator(getStories => (
+    <SbContainer alignItems="center" justifyContent="center" padding={16}>
+      {getStories()}
+    </SbContainer>
+  ))
+  .add('MenuButton', () => <MenuButton {...StandardProps} />);
