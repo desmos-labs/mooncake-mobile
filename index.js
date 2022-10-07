585bddba0190e8475f6c46dacf78632aef0f2bff
Alessandro Mazzon <alessandro@forbole.com>
2022-10-07 16:23:47 +0200

    fix: main

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 import './shim';
 import './src/assets/locales/i18n';
-import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
