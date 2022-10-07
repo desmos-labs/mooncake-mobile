93ec64fb8edf4fc01362d1c495b99e72e7b86439
Kevin Pang <moistbobo@gmail.com>
2022-10-07 19:46:19 +0800

    fix(DFP-550): Fix LookingForDevices screen behavior when bluetooth is off (#143)
    * fix(DFP-550): Add handling for when bluetooth is off
    
    * docs: add reminder
    
    * refactor(LookingForDevices): Improve BT on/off handling
    
    * chore: PR self review
    
    * fix(DFP-550): Use BluetoothStateManager instead of useInterval, fix permission checking logic
    
    * chore: remove console.log
    
    * chore: more clean up

--- src/hooks/ledger/useStartBleScan.ts
+++ src/hooks/ledger/useStartBleScan.ts
@@ -2,7 +2,6 @@ import TransportBLE from '@ledgerhq/react-native-hw-transport-ble';
 import {useCallback, useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {Linking, Platform} from 'react-native';
-import {BleError, BleErrorCode} from 'react-native-ble-plx';
 import BluetoothStateManager from 'react-native-bluetooth-state-manager';
 
 export enum ScanErrorCause {
@@ -26,7 +25,6 @@ export default function useStartBleScan() {
   >(undefined);
   const [scanning, setScanning] = useState(false);
   const [devices, setDevices] = useState<BleLedger[]>([]);
-  const [scanError, setScanError] = useState<ScanError | undefined>(undefined);
 
   // Clear the subscription when leaving the screen or when staring a new scan.
   useEffect(
@@ -53,132 +51,61 @@ export default function useStartBleScan() {
     setScanSubscription(undefined);
   }, []);
 
+  // The error handling portion of this callback can be simplified
   const scan = useCallback(
     async (durationMs = 10000) => {
-      if (scanError?.cause === ScanErrorCause.PoweredOff) {
+      const state = await BluetoothStateManager.getState();
+
+      if (state === 'PoweredOff') {
         if (Platform.OS === 'ios') {
           await Linking.openURL('App-Prefs:Bluetooth');
         } else {
           await BluetoothStateManager.openSettings();
         }
-        setScanError(undefined);
-      } else if (scanError?.cause === ScanErrorCause.Unauthorized) {
-        if (Platform.OS === 'ios') {
-          await Linking.openURL('app-settings:');
-        } else {
-          await Linking.openSettings();
-        }
-        setScanError(undefined);
       } else {
-        setScanning(true);
-        setDevices([]);
-        setScanError(undefined);
+        setScanSubscription(
+          TransportBLE.listen({
+            complete: () => {
+              setScanning(false);
+            },
+            next: (e: any) => {
+              if (e.type === 'add') {
+                const {id, name} = e.descriptor;
 
-        const state = await BluetoothStateManager.getState();
-
-        if (state === 'PoweredOn') {
-          setScanSubscription(
-            TransportBLE.listen({
-              complete: () => {
-                setScanning(false);
-              },
-              next: (e: any) => {
-                if (e.type === 'add') {
-                  const {id, name} = e.descriptor;
-
-                  setDevices(currentDevices => {
-                    const devicePresent =
-                      currentDevices.find(d => d.id === id) !== undefined;
-                    if (!devicePresent) {
-                      return [
-                        ...currentDevices,
-                        {
-                          id,
-                          name,
-                        },
-                      ];
-                    }
-                    return currentDevices;
-                  });
-                }
-              },
-              error: (error: BleError) => {
-                console.error(
-                  'BLE scan error',
-                  error,
-                  error.errorCode,
-                  error.reason,
-                  error.name,
-                );
-                const errorMessage = error.toString();
-                if (
-                  errorMessage.indexOf('not authorized') > 0 ||
-                  error.errorCode === BleErrorCode.BluetoothUnauthorized ||
-                  error.errorCode === BleErrorCode.ScanStartFailed
-                ) {
-                  let message: string;
-                  if (Platform.OS === 'android') {
-                    if (error.errorCode === BleErrorCode.ScanStartFailed) {
-                      message = t('ble scan request nearby devices permission');
-                    } else {
-                      message = t('ble scan request location permission');
-                    }
-                  } else {
-                    message = t('bluetooth access request');
+                setDevices(currentDevices => {
+                  const devicePresent =
+                    currentDevices.find(d => d.id === id) !== undefined;
+                  if (!devicePresent) {
+                    return [
+                      ...currentDevices,
+                      {
+                        id,
+                        name,
+                      },
+                    ];
                   }
-                  setScanError({
-                    cause: ScanErrorCause.Unauthorized,
-                    message,
-                  });
-                } else {
-                  setScanError({
-                    cause: ScanErrorCause.Unknown,
-                    message: errorMessage,
-                  });
-                }
-                setScanning(false);
-              },
-            }),
-          );
-          setStopStopScanTimeout(
-            setTimeout(() => {
-              stopScan();
-            }, durationMs),
-          );
-        } else if (state === 'PoweredOff') {
-          setScanError({
-            cause: ScanErrorCause.PoweredOff,
-            message: t('turn on bluetooth'),
-          });
-          setScanning(false);
-        } else if (state === 'Unauthorized') {
-          let message: string;
-          if (Platform.OS === 'android') {
-            message = t('ble scan request');
-          } else {
-            message = t('bluetooth access request');
-          }
-          setScanError({
-            cause: ScanErrorCause.Unauthorized,
-            message,
-          });
-          setScanning(false);
-        } else {
-          setScanError({
-            cause: ScanErrorCause.Unknown,
-            message: `Unknown bluetooth state ${state}`,
-          });
-          setScanning(false);
-        }
+                  return currentDevices;
+                });
+              }
+            },
+            error: () => {
+              setScanning(false);
+            },
+          }),
+        );
+        setStopStopScanTimeout(
+          setTimeout(() => {
+            stopScan();
+          }, durationMs),
+        );
       }
     },
-    [stopScan, t, scanError],
+    [stopScan, t],
   );
 
   return {
     scan,
     scanning,
     devices,
-    scanError,
   };
 }
