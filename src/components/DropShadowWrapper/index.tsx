561db126b1d2e5476ec90aca40fb34c082a25111
Kevin Pang <moistbobo@gmail.com>
2022-10-12 16:58:25 +0800

    refactor(DropShadowWrapper): Make it easier to customize inner and outer shadow props

--- src/components/DropShadowWrapper/index.tsx
+++ src/components/DropShadowWrapper/index.tsx
@@ -5,68 +5,73 @@ import {Shadow} from 'react-native-shadow-2';
 import useStyles from './useStyles';
 
 export type Props = ViewProps & {
+  // @deprecated use innerShadowProps instead
   innerStyle?: ViewStyle;
 
   /**
    * Override the base shadow color.
+   * @deprecated use innerShadowProps instead
    */
   customColor?: ColorValue;
 
+  // @deprecated use innerShadowProps instead
   customDistance?: number;
 
   /**
    * Override the overlay shadow color. The Overlay shadow is a secondary
    * shadow that can be used to make the shadow effect appear darker or to put
    * emphasis on the wrapped component.
+   * @deprecated use innerShadowProps instead
    */
   customOverlayColor?: ColorValue;
 
   /**
    * Disable the inner "emphasis" shadow (customOverlayColor will no longer have any effect)
+   * @deprecated use innerShadowProps instead
    */
   disableInnerWrapper?: boolean;
+
+  innerShadowProps?: React.ComponentProps<typeof Shadow>;
+
+  outerShadowProps?: React.ComponentProps<typeof Shadow>;
 };
 
 const DropShadowWrapper: React.FC<Props> = props => {
+  const styles = useStyles();
+  const theme = useTheme();
+
   const {
     children,
-    style,
     innerStyle,
     customColor,
     customDistance,
     customOverlayColor,
     disableInnerWrapper,
+    innerShadowProps,
+    outerShadowProps,
   } = props;
-  const styles = useStyles();
-  const theme = useTheme();
   if (disableInnerWrapper) {
     return (
       <Shadow
-        viewStyle={[style, styles.externalShadow]}
+        viewStyle={[innerStyle, styles.externalShadow]}
         startColor={(customColor as any) || 'rgba(37, 87, 188, 0.1)'}
         distance={40}
         offset={[10, 20]}
-        radius={theme.roundness}>
+        radius={theme.roundness}
+        {...outerShadowProps}>
         {children}
       </Shadow>
     );
   }
 
   return (
-    <Shadow
-      viewStyle={[style, styles.externalShadow]}
-      startColor={(customColor as any) || 'rgba(37, 87, 188, 0.1)'}
-      distance={40}
-      offset={[10, 20]}
-      radius={theme.roundness}>
+    <Shadow {...outerShadowProps}>
       <Shadow
         viewStyle={[innerStyle, styles.innerShadow]}
         startColor={(customOverlayColor as any) || 'rgba(16, 24, 40, 0.04)'}
         distance={customDistance || 6}
-        offset={[0, 1]}
-        // fix for android crash
-        // nested shadows can't have the same radius, it seems
-        radius={theme.roundness + 1}>
+        radius={theme.roundness + 1}
+        {...innerShadowProps}>
         {children}
       </Shadow>
     </Shadow>
