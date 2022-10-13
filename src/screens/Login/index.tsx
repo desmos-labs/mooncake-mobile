baca81a9679087435afed19cecb37ccc98bdbc38
Kevin Pang <moistbobo@gmail.com>
2022-10-13 15:24:04 +0800

    fix(Login): No error message showing (#179)

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -77,9 +77,7 @@ const Login = () => {
         onSuccessFn && onSuccessFn();
       }
     } catch (err: any) {
-      if (err.toString().includes('Incorrect')) {
-        setError(t('error:incorrectPassword'));
-      }
+      setError(t('error:incorrectPassword'));
     } finally {
       setLoading(false);
     }
