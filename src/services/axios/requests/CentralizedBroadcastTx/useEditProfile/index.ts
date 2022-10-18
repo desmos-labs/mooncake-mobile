9ccbab078818cf0eaae9f50e1603108c44ae6d9c
Kevin Pang <moistbobo@gmail.com>
2022-10-18 10:44:47 +0800

    refactor(useEditProfile): Remove try/catch

--- src/services/axios/requests/CentralizedBroadcastTx/useEditProfile/index.ts
+++ src/services/axios/requests/CentralizedBroadcastTx/useEditProfile/index.ts
@@ -1,15 +1,13 @@
 import {MsgSaveProfileEncodeObject} from '@desmoslabs/desmjs';
 import {MsgSaveProfile} from '@desmoslabs/desmjs-types/desmos/profiles/v3/msgs_profile';
 import {GrantEnums} from 'lib/desmos/msgtypes';
-import React, {useCallback} from 'react';
+import {useCallback} from 'react';
 import {encodeAndBroadcastTx} from 'services/axios/requests/CentralizedBroadcastTx';
 
 /**
  * Hook that wraps logic to allow the user to edit their profile.
  */
 const useEditProfile = () => {
-  const [editProfileLoading, setEditProfileLoading] = React.useState(false);
-
   /**
    * @param {ProfileData} profileData An object containing everything related to the Desmos Profile
    * */
@@ -21,33 +19,26 @@ const useEditProfile = () => {
       profileData: Partial<ProfileData>;
       userAddress: string;
     }) => {
-      setEditProfileLoading(true);
-      try {
-        const msg: MsgSaveProfileEncodeObject = {
-          typeUrl: GrantEnums.MsgSaveProfile,
-          value: MsgSaveProfile.fromPartial({
-            dtag: profileData.dtag,
-            nickname: profileData.nickname,
-            bio: profileData.bio,
-            profilePicture: profileData.profile_pic,
-            coverPicture: profileData.cover_pic,
-            creator: userAddress,
-          }),
-        };
+      const msg: MsgSaveProfileEncodeObject = {
+        typeUrl: GrantEnums.MsgSaveProfile,
+        value: MsgSaveProfile.fromPartial({
+          dtag: profileData.dtag,
+          nickname: profileData.nickname,
+          bio: profileData.bio,
+          profilePicture: profileData.profile_pic,
+          coverPicture: profileData.cover_pic,
+          creator: userAddress,
+        }),
+      };
 
-        const result = await encodeAndBroadcastTx({msgs: [msg]});
+      const result = await encodeAndBroadcastTx({msgs: [msg]});
 
-        return result;
-      } catch (err: any) {
-        throw new Error(err.toString());
-      } finally {
-        setEditProfileLoading(false);
-      }
+      return result;
     },
     [],
   );
 
-  return {editProfile, editProfileLoading};
+  return {editProfile};
 };
 
 export default useEditProfile;
