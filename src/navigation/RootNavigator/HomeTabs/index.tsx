f5e7d14f527cdcffd858fafaefa28a61489de72f
Kevin Pang <moistbobo@gmail.com>
2022-10-20 22:31:45 +0800

    fix: HomeTabs top padding

--- src/navigation/RootNavigator/HomeTabs/index.tsx
+++ src/navigation/RootNavigator/HomeTabs/index.tsx
@@ -45,7 +45,11 @@ const HomeTabs = () => {
 
   return (
     <SafeAreaView
-      style={{flex: 1, backgroundColor: theme.colors.background}}
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        paddingTop: 16,
+      }}
       edges={['top', 'bottom']}>
       <Tab.Navigator tabBar={renderTabBar}>
         <Tab.Screen
