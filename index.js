bbd48e6143bdc3552eb9c5cec0dfd4fc9f11106c
Kevin Pang <moistbobo@gmail.com>
2022-10-24 15:03:28 +0800

    refactor(MaterialButton): Pressable -> Touchable Opacity

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  */
 import './shim';
 import './src/assets/locales/i18n';
-import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import StorybookUIRoot from './storybook';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
