332ee44fc88dc25485aced2f7344045cf596c54a
Alessandro Mazzon <alessandro@forbole.com>
2022-10-25 08:46:45 +0200

    fix: added a connect button inside empty component

--- src/screens/ManageConnectedApps/index.tsx
+++ src/screens/ManageConnectedApps/index.tsx
@@ -3,6 +3,7 @@ import {useNavigation} from '@react-navigation/native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {connectedAppsState} from '@recoil/connectedApps';
 import {modalSuccess} from 'assets/images';
+import Button from 'components/Button';
 import DView from 'components/DView';
 import GradientBorder from 'components/GradientBorder';
 import Spacer from 'components/Spacer';
@@ -107,6 +108,15 @@ const ManageConnectedApps = () => {
     return (
       <Spacer paddingTop={140}>
         <NoAppConnections />
+        <View style={styles.buttonContainer}>
+          <Button
+            color={theme.colors.surfaceBlack}
+            onPress={() => navigate(ROUTES.CONNECT_APP, {mode: 'connect'})}
+            mode="contained"
+            labelStyle={styles.buttonStyle}>
+            {t('connect app')}
+          </Button>
+        </View>
       </Spacer>
     );
   }, []);
