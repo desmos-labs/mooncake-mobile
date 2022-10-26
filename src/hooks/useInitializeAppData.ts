7f7289fa90b798f276a9248e9334ee47fc02784b
Alessandro Mazzon <alessandro@forbole.com>
2022-10-26 11:01:46 +0200

    hotfix: fixed send tips issues and tests

--- src/hooks/useInitializeAppData.ts
+++ src/hooks/useInitializeAppData.ts
@@ -8,6 +8,7 @@ import {useSetRecoilState} from 'recoil';
 import appSettingsState from '@recoil/settings';
 import {useGetProfileParams} from '@recoil/profileParams';
 import {useGetButterConfig} from '@recoil/butterConfigState';
+import GetContractsConfig from 'services/graphql/queries/GetContractsConfig';
 import GetRegisteredReactions from 'services/graphql/queries/GetRegisteredReactions';
 import {useInitializePostParams} from '@recoil/postParamsState';
 import GetRegisteredReports from 'services/graphql/queries/GetRegisteredReports';
@@ -32,6 +33,14 @@ const useInitializeAppData = () => {
       fetchPolicy: 'no-cache',
     },
   );
+
+  const {data: contractsConfig, loading: contractsConfigLoading} = useQuery(
+    GetContractsConfig,
+    {
+      fetchPolicy: 'no-cache',
+    },
+  );
+
   const profileParams = useGetProfileParams();
   useInitializePostParams();
   useInitializeAxios();
@@ -55,7 +64,8 @@ const useInitializeAppData = () => {
     if (
       profileParams &&
       !registeredReportsLoading &&
-      !registeredReactionsLoading
+      !registeredReactionsLoading &&
+      !contractsConfigLoading
     ) {
       setAppSettings(prev => ({
         ...prev,
@@ -63,12 +73,13 @@ const useInitializeAppData = () => {
         currentTimezone: RNLocalize.getTimeZone(),
         registeredReactions: registeredReactions?.subspace_registered_reaction,
         registeredReports: registeredReports?.subspace_report_reason,
+        contractsConfig: contractsConfig?.contract,
         dataInitialized: true,
         currentChain:
-          EnvConfig.CHAIN_ID === 'mainnet' ? DesmosMainnet : DesmosTestnet,
+          EnvConfig.CHAIN === 'mainnet' ? DesmosMainnet : DesmosTestnet,
       }));
     }
-  }, [profileParams, registeredReactions, registeredReports]);
+  }, [profileParams, registeredReactions, registeredReports, contractsConfig]);
 };
 
 export default useInitializeAppData;
