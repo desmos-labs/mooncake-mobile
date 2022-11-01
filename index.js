1ba533d807651aa2704f2872e466f04acc3e4a16
Kevin Pang <moistbobo@gmail.com>
2022-11-01 15:04:49 +0800

    chore: switch to use:main

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 import './shim';
 import './src/assets/locales/i18n';
+import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
-import StorybookUIRoot from './storybook';
 
-AppRegistry.registerComponent(appName, () => StorybookUIRoot);
+AppRegistry.registerComponent(appName, () => App);
