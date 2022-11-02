70ae25d532e83e3a09f47cd43567111df24128fc
Kevin Pang <moistbobo@gmail.com>
2022-11-02 17:28:28 +0800

    docs(usePendingPosts): add comments

--- src/recoil/pendingTx/pendingPosts.ts
+++ src/recoil/pendingTx/pendingPosts.ts
@@ -8,21 +8,47 @@ export const pendingPostsState = atom<PendingPost[]>({
   default: [],
 });
 
+/**
+ * Check if an array of PostItems contains a given txHash
+ * @param {string} txHash - The txHash to search for.
+ * @param {PostItem[]} posts - An array of posts to search for the txHash in.
+ * @returns {PostItem|undefined} - The matching post data or undefined if no match.
+ */
 const isTxHashInLatestPost = (
   txHash: string,
-  latestPosts: PostItem[],
+  posts: PostItem[],
 ): PostItem | undefined =>
-  latestPosts.find(x => {
+  posts.find(x => {
     const txHashes = x.transactions.map(y => y.hash);
     return txHashes.includes(txHash);
   });
 
+/**
+ * A hook that wraps all logic involving pending posts.
+ * Posts on Butter can be pending or non-pending. Pending posts are temporary, local data structures that contain
+ * just enough data to render a placeholder PostCard on the Home page to indicate that the post is being broadcast
+ * onto the chain.
+ */
 const usePendingPosts = () => {
   const [pendingPosts, setPendingPosts] = useRecoilState(pendingPostsState);
   const latestPostsByUser = useRecoilValue(latestPostsByUserState);
 
   const {setPosts} = usePostsFamily(POST_TYPE.DISCOVER);
 
+  /**
+   * An effect that runs whenever the user's latest posts have changed.
+   * See usePollLatestPostsByUser hook for the implementation.
+   */
+  React.useEffect(() => {
+    if (latestPostsByUser && latestPostsByUser.length > 0) {
+      syncPendingPosts(latestPostsByUser, pendingPosts);
+    }
+  }, [latestPostsByUser]);
+
+  /**
+   * A React callback that checks if a pending post has made it onto the chain, and
+   * batches post data to be "transferred" from pending to broadcasted.
+   */
   const syncPendingPosts = React.useCallback(
     (newPosts: PostItem[], _pendingPosts: PendingPost[]) => {
       const postsToTransfer: PostItem[] = [];
@@ -42,11 +68,6 @@ const usePendingPosts = () => {
     [],
   );
 
-  React.useEffect(() => {
-    if (latestPostsByUser && latestPostsByUser.length > 0) {
-      syncPendingPosts(latestPostsByUser, pendingPosts);
-    }
-  }, [latestPostsByUser]);
   /**
    * Add a new pending relationship to recoil state.
    * @param {PendingPost} newPost - The new relationship to be added.
