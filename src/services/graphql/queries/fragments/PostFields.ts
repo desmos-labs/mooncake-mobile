0b354dad71815d7409d5898f84cac66d3b44d22b
Kevin Pang <moistbobo@gmail.com>
2022-11-04 18:45:56 +0800

    refactor: PostFields fragment

--- src/services/graphql/queries/fragments/PostFields.ts
+++ src/services/graphql/queries/fragments/PostFields.ts
@@ -38,7 +38,15 @@ const POST_FIELDS = gql`
     }
     transactions {
       hash
-    }
+    }  
+    replies: references(where: {type: {_eq: "POST_REFERENCE_TYPE_REPLY"}}) {
+      type
+      post {
+        id
+      }
+      reference {
+        id
+      }
     repliesCount: referees_aggregate(
       where: {type: {_eq: "POST_REFERENCE_TYPE_REPLY"}}
     ) {
