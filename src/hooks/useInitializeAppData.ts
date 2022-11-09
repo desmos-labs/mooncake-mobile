ece81eeef1bf53b3bdb8e051c6496b56670d719d
Alessandro Mazzon <alessandro@forbole.com>
2022-11-09 10:24:06 +0100

    fix: tips

--- src/hooks/useInitializeAppData.ts
+++ src/hooks/useInitializeAppData.ts
@@ -39,7 +39,7 @@ const useInitializeAppData = () => {
     {
       variables: {
         config: {
-          subspace_id: '5',
+          subspace_id: EnvConfig.APP_SUBSPACE_ID,
         },
       },
       fetchPolicy: 'no-cache',
