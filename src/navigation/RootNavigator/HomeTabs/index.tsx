619c737fcbe1c8e292df3961c172355f44a54745
Kevin Pang <moistbobo@gmail.com>
2022-11-18 10:25:30 +0800

    fix: Refresh session logic
    - Move refresh session and token restore logic into its own hook

--- src/navigation/RootNavigator/HomeTabs/index.tsx
+++ src/navigation/RootNavigator/HomeTabs/index.tsx
@@ -7,14 +7,10 @@ import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useTheme} from 'react-native-paper';
 import LoadingOverlay from 'components/LoadingOverlay';
 import {MaterialTopTabBarProps} from '@react-navigation/material-top-tabs/lib/typescript/src/types';
-import RefreshSession from 'services/axios/requests/RefreshSession';
-import {MMKVKEYS, useMMKVStorage} from 'lib/MMKVStorage';
-import {useNavigation} from '@react-navigation/native';
-import {StackScreenProps} from '@react-navigation/stack';
-import {RootNavigatorParamList} from 'navigation/RootNavigator';
 import useActiveAccount from 'hooks/useActiveAccount';
 import ThemedLottieView from 'components/ThemedLottieView';
 import {broadcastAnim} from 'assets/animations';
+import useRefreshSession from 'hooks/useRefreshSession';
 
 export type HomeTabsParamList = {
   [ROUTES.HOME_DISCOVER]: HomeParams;
@@ -24,14 +20,11 @@ export type HomeTabsParamList = {
 
 const Tab = createMaterialTopTabNavigator<HomeTabsParamList>();
 
-type NavProps = StackScreenProps<RootNavigatorParamList, ROUTES.HOME_TABS>;
-
 const HomeTabs = () => {
   const theme = useTheme();
   const [loading, setLoading] = React.useState(false);
-  const [bearerToken] = useMMKVStorage<string>(MMKVKEYS.REST_AUTH_TOKEN);
-  const {replace} = useNavigation<NavProps['navigation']>();
   const {top} = useSafeAreaInsets();
+  const {refreshSession} = useRefreshSession();
 
   const {profileData} = useActiveAccount();
 
@@ -43,16 +36,6 @@ const HomeTabs = () => {
 
   // Refresh the token if we have one, otherwise have the user relog
   React.useEffect(() => {
-    const refreshSession = async () => {
-      if (bearerToken) {
-        try {
-          await RefreshSession();
-        } catch {
-          replace(ROUTES.LOGIN);
-        }
-      } else replace(ROUTES.LOGIN);
-    };
-
     refreshSession();
   }, []);
 
