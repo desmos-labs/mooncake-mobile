369988cf5d966becae2c83ba9db38475265b1e37
Kevin Pang <moistbobo@gmail.com>
2022-11-23 14:46:44 +0800

    refactor(ActionAuthorization): Text and UI adjustments

---
+++ src/components/TextBullet/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View} from 'react-native';
+import Typography from 'components/Typography';
+import useStyles from './useStyles';
+
+type Props = {
+  textArr: string[];
+};
+
+/**
+ * A component that renders an array of strings as individual bullet points
+ */
+const TextBullet = ({textArr}: Props) => {
+  const styles = useStyles();
+  return (
+    <View style={styles.outerContainer}>
+      {textArr.map(x => (
+        <View style={styles.innerContainer}>
+          <View style={styles.bullet} />
+
+          <Typography.Body5>{x}</Typography.Body5>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default TextBullet;
