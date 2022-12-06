193904b8cfa2ad69f16c7da388551c2461435593
Alessandro Mazzon <alessandro@forbole.com>
2022-12-06 16:42:21 +0100

    fix: activities

--- src/screens/Activities/index.tsx
+++ src/screens/Activities/index.tsx
@@ -112,7 +112,7 @@ const Activities = () => {
       backgroundColor={theme.colors.white}
       style={styles.container}>
       <Typography.H3>{t('activities')}</Typography.H3>
-      {notificationsData.length > 0 && !notificationsLoading ? (
+      {data.notification.length >= 0 && !notificationsLoading ? (
         <SectionList
           keyExtractor={(item, index) => item.timestamp + index}
           refreshing={notificationsLoading}
@@ -123,9 +123,7 @@ const Activities = () => {
           ListEmptyComponent={EmptyActivities}
           sections={notificationsData}
           renderItem={renderNotification}
-          ListFooterComponent={
-            notificationsLoading ? <ActivityIndicator /> : null
-          }
+          ListFooterComponent={globalLoading ? <ActivityIndicator /> : null}
           onEndReached={() => {
             notificationsFetchMore({
               variables: {
