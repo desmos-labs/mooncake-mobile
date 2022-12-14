b9c4ed7cbc0258016e2044a76e515e2c1548ed18
Alessandro Mazzon <alessandro@forbole.com>
2022-12-14 17:01:49 +0100

    chore: re-organized code

--- index.js
+++ index.js
@@ -7,45 +7,13 @@ import './src/assets/locales/i18n';
 import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
-import notifee from '@notifee/react-native';
+import {createLocalNotification} from 'lib/NotificationsUtils/notificationsUtils';
 import App from './App';
 import {name as appName} from './app.json';
 import AppSilent from './AppSilent';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  const channelId = await notifee.createChannel({
-    id: 'default',
-    name: 'Default Channel',
-    sound: 'default',
-    vibration: true,
-  });
-
-  if (
-    remoteMessage.data?.type !== 'transaction_success' &&
-    remoteMessage.data?.type !== 'transaction_fail'
-  ) {
-    await notifee.displayNotification({
-      title: remoteMessage.data?.notification_title,
-      body: remoteMessage.data?.notification_body,
-      android: {
-        channelId,
-        smallIcon: 'ic_small_icon',
-        pressAction: {
-          id: 'default',
-        },
-      },
-      ios: {
-        interruptionLevel: 'active',
-        foregroundPresentationOptions: {
-          badge: true,
-          sound: true,
-          banner: true,
-          list: true,
-        },
-        sound: 'default',
-      },
-    });
-  }
+  createLocalNotification(remoteMessage);
 });
 
 function HeadlessCheck({isHeadless}) {
--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,11 +1,12 @@
-import notifee from '@notifee/react-native';
 import messaging from '@react-native-firebase/messaging';
-import ToastConfig from 'config/ToastConfig';
 import useFindPendingTx from 'hooks/useFindPendingTx';
+import {
+  createLocalNotification,
+  createTransactionSnackbar,
+} from 'lib/NotificationsUtils/notificationsUtils';
 import _ from 'lodash';
 import {useEffect} from 'react';
 import {useToast} from 'react-native-toast-notifications';
-import {encodeAndBroadcastTx} from 'services/axios/requests/CentralizedBroadcastTx';
 
 const useNotifications = () => {
   const toast = useToast();
@@ -14,53 +15,13 @@ const useNotifications = () => {
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       const txHash = _.get(remoteMessage, 'data.tx_hash');
-      if (remoteMessage.data?.type === 'transaction_success') {
-        toast.show('Transaction success!', {
-          type: ToastConfig.SUCCESS,
-        });
-      } else if (remoteMessage.data?.type === 'transaction_fail') {
-        toast.show('Transaction failed!', {
-          type: ToastConfig.ERROR,
-          // @ts-ignore
-          onPressRetry: () => {
-            // find the matching txHash and rebroadcast its message
-            const pendingTx = findPendingTxByHash(txHash);
-            if (pendingTx) {
-              const {msg} = pendingTx;
-              encodeAndBroadcastTx({msgs: [msg]});
-            }
-          },
-        });
-      } else {
-        const channelId = await notifee.createChannel({
-          id: 'default',
-          name: 'Default Channel',
-          sound: 'default',
-          vibration: true,
-        });
-
-        await notifee.displayNotification({
-          title: remoteMessage.data?.notification_title,
-          body: remoteMessage.data?.notification_body,
-          android: {
-            channelId,
-            smallIcon: 'ic_small_icon',
-            pressAction: {
-              id: 'default',
-            },
-          },
-          ios: {
-            interruptionLevel: 'active',
-            foregroundPresentationOptions: {
-              badge: true,
-              sound: true,
-              banner: true,
-              list: true,
-            },
-            sound: 'default',
-          },
-        });
-      }
+      createTransactionSnackbar(
+        remoteMessage,
+        toast,
+        txHash,
+        findPendingTxByHash,
+      );
+      createLocalNotification(remoteMessage);
     });
     return unsubscribe;
   }, [toast]);
