793f0b620454705316b5df9c621e333ad89a33d4
Alessandro Mazzon <alessandro@forbole.com>
2022-12-14 10:19:58 +0100

    fix: background handler

--- index.main.js
+++ index.main.js
@@ -5,7 +5,35 @@ import './shim';
 import './src/assets/locales/i18n';
 import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+    sound: 'default',
+    vibration: true,
+  });
+
+  if (remoteMessage.notification) {
+    await notifee.displayNotification({
+      title: remoteMessage.notification?.title,
+      body: remoteMessage.notification?.body,
+      android: {
+        channelId,
+        smallIcon: 'ic_small_icon',
+        pressAction: {
+          id: 'default',
+        },
+      },
+      ios: {
+        sound: 'default',
+      },
+    });
+  }
+});
 
 AppRegistry.registerComponent(appName, () => App);
