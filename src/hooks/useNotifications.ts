c6e520538c81ff42444a33a834c6a70d78029262
Alessandro Mazzon <alessandro@forbole.com>
2022-12-15 10:28:48 +0100

    fix: hidden notifications when app is open

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -5,25 +5,43 @@ import {
   createTransactionSnackbar,
 } from 'lib/NotificationsUtils/notificationsUtils';
 import _ from 'lodash';
-import {useEffect} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {AppState} from 'react-native';
 import {useToast} from 'react-native-toast-notifications';
 
 const useNotifications = () => {
   const toast = useToast();
   const {findPendingTxByHash} = useFindPendingTx();
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      const txHash = _.get(remoteMessage, 'data.tx_hash');
-      createTransactionSnackbar(
-        remoteMessage,
-        toast,
-        txHash,
-        findPendingTxByHash,
-      );
-      createLocalNotification(remoteMessage);
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
     });
-    return unsubscribe;
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Checking if the app is active, if so we do not want to send the user notifications
+    // We will be able to use this onMessage to handle different type of notifications, maybe create a snackbar instead
+    if (appStateVisible !== 'active') {
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        const txHash = _.get(remoteMessage, 'data.tx_hash');
+        createTransactionSnackbar(
+          remoteMessage,
+          toast,
+          txHash,
+          findPendingTxByHash,
+        );
+        createLocalNotification(remoteMessage);
+      });
+      return unsubscribe;
+    }
   }, [toast]);
 };
 
