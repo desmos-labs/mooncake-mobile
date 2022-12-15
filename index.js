2fdd58186be414df191904727ddbcbed5294f86d
Alessandro Mazzon <alessandro@forbole.com>
2022-12-15 15:40:44 +0100

    fix: Notifications refactor (#265)
    ## Notifications refactor
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    - Now notifications are sent only with the `data` property, this way we
    can handle behaviors and interactions, and customization
    - Added a handler for background notifications
    - Added sounds, lights, icon, vibration on Android
    - Added sound and vibration on iOS
    
    At the moment the notification will be displayed even if the app is
    open, I think we can change this behavior and hide the notification
    while the user is using the application
    **Edit: hidden notifications when the app is in an `active` state**
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 /**
  * @format
  */
+import React from 'react';
 import './shim';
 import './src/assets/locales/i18n';
 import 'fastestsmallesttextencoderdecoder';
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import {createLocalNotification} from 'lib/NotificationsUtils/notificationsUtils';
 import App from './App';
 import {name as appName} from './app.json';
+import AppSilent from './AppSilent';
 
-AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  createLocalNotification(remoteMessage);
+});
+
+function HeadlessCheck({isHeadless}) {
+  if (isHeadless) {
+    console.log('Headless');
+    return <AppSilent />;
+  }
+
+  return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
