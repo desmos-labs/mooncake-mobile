04adc1ba8d7d12d63a03c01a66a0a7f6569a56fb
Kevin Pang <moistbobo@gmail.com>
2022-12-22 15:43:28 +0900

    chore: remove pendingRelationships.ts and associated files

--- src/recoil/following.ts
+++ src/recoil/following.ts
@@ -1,21 +1,10 @@
-import {useCallback, useEffect} from 'react';
-import {
-  atom,
-  selector,
-  selectorFamily,
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-} from 'recoil';
-import {useLazyQuery, useQuery} from '@apollo/client';
+import {useCallback} from 'react';
+import {atom, selector, selectorFamily, useSetRecoilState} from 'recoil';
+import {useLazyQuery} from '@apollo/client';
 import GetFollowedUsersForAddress, {
   GetFollowedUsersForAddressData,
 } from 'services/graphql/queries/GetFollowedUsersForAddress';
-import useActiveAccount from 'hooks/useActiveAccount';
-import usePendingRelationships, {
-  pendingRelationshipsState,
-} from '@recoil/pendingTx/pendingRelationships';
-import EnvConfig from 'config/EnvConfig';
+
 import {
   hasOptimisticFollow,
   hasOptimisticUnfollow,
@@ -83,51 +72,4 @@ export const useGetFollowingForAddress = (address: string) => {
   return {
     updateFollowing,
   };
-};
-
-/**
- * Get the list of followed accounts for the active account
- */
-export const useGetFollowingPolling = () => {
-  const {activeAddress} = useActiveAccount();
-  const [, setFollowing] = useRecoilState(followingState);
-  const {syncPendingRelationships} = usePendingRelationships();
-  const pendingRelationships = useRecoilValue(pendingRelationshipsState);
-
-  const {data, startPolling, stopPolling, refetch} =
-    useQuery<GetFollowedUsersForAddressData>(GetFollowedUsersForAddress, {
-      variables: {
-        userAddress: activeAddress,
-      },
-      fetchPolicy: 'no-cache',
-    });
-
-  useEffect(() => {
-    if (!data) return;
-    const {user_relationship} = data;
-
-    const newFollowing = user_relationship
-      .map(x => x.counterparty)
-      .filter(d => !!d);
-
-    setFollowing(newFollowing);
-
-    syncPendingRelationships(newFollowing.map(x => x.address));
-  }, [JSON.stringify(data)]);
-
-  useEffect(() => {
-    if (activeAddress) {
-      refetch({
-        userAddress: activeAddress,
-      });
-    }
-  }, [activeAddress]);
-
-  useEffect(() => {
-    if (pendingRelationships.length > 0) {
-      startPolling(EnvConfig.POLLING_INTERVAL);
-    } else {
-      stopPolling();
-    }
-  }, [pendingRelationships.length]);
 };
