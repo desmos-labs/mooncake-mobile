3dd11c69fefbd56b4d974401da0f61769b100810
Alessandro Mazzon <alessandro@forbole.com>
2022-12-29 18:48:27 +0100

    fix(DFP-879): Impact points fix (#276)
    ## Impact points fix
    
    Closes: DFP-879
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    Little follow up to the backend fix related to DFP-879
    - Fixed the points display, the invites number check and a typo
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

--- src/screens/Invites/index.tsx
+++ src/screens/Invites/index.tsx
@@ -65,6 +65,15 @@ const Invites = () => {
     }
   }, [data, activeAddress]);
 
+  const requiredPoints = useMemo(() => {
+    if (numInvitesGenerated === undefined) {
+      return undefined;
+    }
+    if (numInvitesGenerated === 0) return 0;
+    if (numInvitesGenerated === 1) return 100;
+    if (numInvitesGenerated >= 2) return 150;
+  }, [numInvitesGenerated]);
+
   const rightElement = useMemo(() => {
     return (
       <TouchableOpacity onPress={() => navigate(ROUTES.MANAGE_INVITES)}>
@@ -108,8 +117,8 @@ const Invites = () => {
         setInviteLink(response.link);
         setInviteGenerated(true);
       }
-    } catch (e) {
-      toast.show('Max amount of invites already reached', {
+    } catch (e: any) {
+      toast.show(e.response.data.toString(), {
         type: ToastConfig.ERROR_NO_RETRY,
       });
       setInviteGenerated(false);
@@ -168,7 +177,9 @@ const Invites = () => {
         shareComponent
       ) : (
         <Button
-          disabled={!numInvitesGenerated || numInvitesGenerated === 3}
+          disabled={
+            numInvitesGenerated === undefined || numInvitesGenerated === 3
+          }
           onPress={generateInvite}
           loading={generationLoading}
           color={theme.colors.surfaceBlack}
@@ -180,10 +191,14 @@ const Invites = () => {
       <Spacer paddingVertical={theme.spacing.m} />
       <View style={{alignItems: 'center'}}>
         <View style={styles.rowCenter}>
-          <Typography.Subtitle2>
-            {t('points', {number: 0})}
-          </Typography.Subtitle2>
-          <Typography.Body5> {t('requried')}</Typography.Body5>
+          {requiredPoints !== undefined ? (
+            <Typography.Subtitle2>
+              {t('points', {number: requiredPoints})}
+            </Typography.Subtitle2>
+          ) : (
+            <ActivityIndicator />
+          )}
+          <Typography.Body5> {t('required')}</Typography.Body5>
           <ImageButton
             onPress={() => navigate(ROUTES.IMPACT_POINTS_MODAL)}
             image={infoIcon}
@@ -193,7 +208,7 @@ const Invites = () => {
         <Spacer paddingTop={6} />
         <View style={styles.rowCenter}>
           <Image source={inviteUserIcon} style={styles.iconRight} />
-          {numInvitesGenerated ? (
+          {numInvitesGenerated !== undefined ? (
             <Typography.Body6 style={{color: theme.colors.midGrey}}>
               {t('invites shared', {number: numInvitesGenerated})}
             </Typography.Body6>
