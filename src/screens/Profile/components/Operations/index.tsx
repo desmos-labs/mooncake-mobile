aed19997fd0cb8d9f6c65e86cab55219f7115a7f
Alessandro Mazzon <alessandro@forbole.com>
2023-01-02 16:55:58 +0100

    feat: operations screen

--- src/screens/Profile/components/Operations/index.tsx
+++ src/screens/Profile/components/Operations/index.tsx
@@ -1,14 +1,30 @@
+import {convertCoin} from '@desmoslabs/desmjs';
 import {useRoute} from '@react-navigation/native';
 import {StackScreenProps} from '@react-navigation/stack';
+import {
+  addReactionTxIcon,
+  createPostTxIcon,
+  defaultProfilePic,
+  editProfileTxIcon,
+  emptyPostsIcon,
+} from 'assets/images';
 import DView from 'components/DView';
 import Spacer from 'components/Spacer';
 import TopBar from 'components/TopBar';
 import Typography from 'components/Typography';
 import {RootNavigatorParamList} from 'navigation/RootNavigator';
 import ROUTES from 'navigation/routes';
-import React from 'react';
+import React, {useCallback, useMemo} from 'react';
 import {useTranslation} from 'react-i18next';
+import {
+  ActivityIndicator,
+  ListRenderItemInfo,
+  SectionList,
+  View,
+} from 'react-native';
+import FastImage from 'react-native-fast-image';
 import {useTheme} from 'react-native-paper';
+import TxComponent from 'screens/Profile/components/Operations/components/TxComponent';
 import useHooks from './useHooks';
 import useStyles from './useStyles';
 
@@ -23,7 +39,90 @@ const Operations = () => {
   const theme = useTheme();
   const styles = useStyles();
   const {params} = useRoute<NavProps['route']>();
-  const {convertedBalance} = useHooks(params.address);
+  const {
+    convertedBalance,
+    operationsData,
+    pastActionsData,
+    currentChain,
+    operationsDataFetchMore,
+    operationsDataLoading,
+    operationsDataRefetch,
+  } = useHooks(params.address);
+
+  const getCorrectTitle = useCallback((msgType: string) => {
+    switch (msgType) {
+      case 'desmos.posts.v2.MsgCreatePost':
+        return t('create comment post');
+      case 'desmos.relationships.v1.MsgCreateRelationship':
+        return t('follow user');
+      case 'desmos.relationships.v1.MsgDeleteRelationship':
+        return t('unfollow user');
+      case 'desmos.reactions.v1.MsgAddReaction':
+        return t('add reaction');
+      case 'desmos.reactions.v1.MsgRemoveReaction':
+        return t('remove reaction');
+      case 'desmos.profiles.v3.MsgSaveProfile':
+        return t('edit profile');
+      default:
+        return 'Not mapped';
+    }
+  }, []);
+
+  const getCorrectImage = useCallback((msgType: string) => {
+    switch (msgType) {
+      case 'desmos.posts.v2.MsgCreatePost':
+        return createPostTxIcon;
+      case 'desmos.relationships.v1.MsgCreateRelationship':
+      case 'desmos.relationships.v1.MsgDeleteRelationship':
+        return editProfileTxIcon;
+      case 'desmos.reactions.v1.MsgAddReaction':
+        return addReactionTxIcon;
+      case 'desmos.profiles.v3.MsgSaveProfile':
+        return editProfileTxIcon;
+      default:
+        return defaultProfilePic;
+    }
+  }, []);
+
+  const EmptyOperations = useMemo(() => {
+    return operationsDataLoading ? null : (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <FastImage
+          resizeMode="contain"
+          source={emptyPostsIcon}
+          style={{
+            width: 72,
+            height: 72,
+            marginBottom: theme.spacing.s,
+          }}
+        />
+        <Typography.Body5>{t('no operations')}</Typography.Body5>
+      </View>
+    );
+  }, [t, operationsDataLoading]);
+
+  const renderTx = React.useCallback(
+    ({item}: ListRenderItemInfo<any>) => {
+      const title = getCorrectTitle(item.type);
+      const image = getCorrectImage(item.type);
+      const fees = convertCoin(item.fees[0], 6, currentChain.currencies);
+      return (
+        <TxComponent
+          timestamp={item.timestamp}
+          fees={parseFloat(fees?.amount || '0').toFixed(4)}
+          title={title}
+          image={image}
+          chain={currentChain}
+        />
+      );
+    },
+    [currentChain],
+  );
 
   return (
     <DView
@@ -40,26 +139,51 @@ const Operations = () => {
       </Typography.H2>
       <Spacer paddingVertical={theme.spacing.s} />
       <Typography.H5>{t('operations')}</Typography.H5>
-      <Spacer paddingVertical={theme.spacing.s} />
-      <Typography.Body7>Coming soon...</Typography.Body7>
-      {/*      <SectionList
-        style={{flex: 1}}
-        contentContainerStyle={{flexGrow: 1}}
-        showsVerticalScrollIndicator={false}
-        sections={operationsData}
-        renderItem={renderNotification}
-        renderSectionHeader={({section: {section}}) => (
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: theme.colors.white,
-              paddingTop: theme.spacing.m,
-              paddingBottom: theme.spacing.s,
-            }}>
-            <Typography.Button2>{section}</Typography.Button2>
-          </View>
-        )}
-      /> */}
+      {pastActionsData?.messages_by_address?.length >= 0 &&
+      !operationsDataLoading ? (
+        <SectionList
+          refreshing={operationsDataLoading}
+          onRefresh={operationsDataRefetch}
+          style={{flex: 1}}
+          contentContainerStyle={{flexGrow: 1}}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={EmptyOperations}
+          sections={operationsData}
+          renderItem={renderTx}
+          onEndReached={() => {
+            operationsDataFetchMore({
+              variables: {
+                offset: pastActionsData?.messages_by_address.length,
+              },
+              updateQuery: (prev, {fetchMoreResult}) => {
+                if (!fetchMoreResult) {
+                  return prev;
+                }
+                return {
+                  ...prev,
+                  messages_by_address: [
+                    ...prev.messages_by_address,
+                    ...fetchMoreResult.messages_by_address,
+                  ],
+                };
+              },
+            });
+          }}
+          renderSectionHeader={({section: {section}}) => (
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: theme.colors.white,
+                paddingTop: theme.spacing.m,
+                paddingBottom: theme.spacing.s,
+              }}>
+              <Typography.Button2>{section}</Typography.Button2>
+            </View>
+          )}
+        />
+      ) : (
+        <ActivityIndicator />
+      )}
     </DView>
   );
 };
