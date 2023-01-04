6eb4c8e89397b1765b6e578caa3628a8a92d0729
Kevin Pang <moistbobo@gmail.com>
2023-01-04 18:29:54 +0900

    feat(DFP-874): Local storage for optimistic posts

--- src/recoil/latestPostsByUser.ts
+++ src/recoil/latestPostsByUser.ts
@@ -1,72 +1,28 @@
-import React, {useCallback} from 'react';
-import {
-  atom,
-  useRecoilValue,
-  useResetRecoilState,
-  useSetRecoilState,
-} from 'recoil';
-import {useLazyQuery, useQuery} from '@apollo/client';
+import {useCallback} from 'react';
+import {atom, useSetRecoilState} from 'recoil';
+import {useLazyQuery} from '@apollo/client';
 import GetLastPostsByAddress from 'services/graphql/queries/GetLastPostsByAddress';
 import EnvConfig from 'config/EnvConfig';
 import useActiveAccount from 'hooks/useActiveAccount';
-import {
-  PendingPostEnum,
-  pendingPostsState,
-} from '@recoil/pendingTx/pendingPosts';
+
 import {useSyncPendingPosts} from 'hooks/usePendingPosts';
+import {getMMKV, MMKVKEYS, setMMKV} from 'lib/MMKVStorage';
 
 export const latestPostsByUserState = atom<PostItem[]>({
   key: 'latestPosts',
-  default: [],
+  default: getMMKV(MMKVKEYS.PENDING_POSTS) || [],
+  effects: [
+    ({onSet}) => {
+      onSet(newValue => {
+        setMMKV(MMKVKEYS.PENDING_POSTS, newValue);
+      });
+    },
+  ],
 });
 
 /**
- * A hook that manages logic related to polling the user's latest posts
- * @param {number} limit - The amount of posts to poll
+ * Get the last 5 posts from the current user's active address
  */
-const usePollLatestPostsByUser = (limit: number) => {
-  const {activeAddress} = useActiveAccount();
-  const setLatestPostsByUser = useSetRecoilState(latestPostsByUserState);
-  const resetLatestPosts = useResetRecoilState(latestPostsByUserState);
-  const pendingPosts = useRecoilValue(pendingPostsState(PendingPostEnum.POST));
-  useSyncPendingPosts();
-
-  const {data, startPolling, stopPolling, loading} = useQuery(
-    GetLastPostsByAddress,
-    {
-      variables: {
-        limit,
-        subspaceID: EnvConfig.APP_SUBSPACE_ID,
-        user: activeAddress!,
-        reaction: {
-          '@type': '/desmos.reactions.v1.RegisteredReactionValue',
-          registered_reaction_id: 9,
-        },
-      },
-      notifyOnNetworkStatusChange: true,
-      fetchPolicy: 'no-cache',
-    },
-  );
-
-  React.useEffect(() => {
-    if (!loading && data) {
-      const {post} = data;
-      setLatestPostsByUser(post);
-    }
-  }, [JSON.stringify(data)]);
-
-  React.useEffect(() => {
-    if (pendingPosts.length > 0) {
-      startPolling(EnvConfig.POLLING_INTERVAL);
-    } else {
-      resetLatestPosts();
-      stopPolling();
-    }
-  }, [pendingPosts.length]);
-};
-
-export default usePollLatestPostsByUser;
-
 export const useGetLatestPostsByActiveAddress = () => {
   const {activeAddress} = useActiveAccount();
   const setLatestPostsByUser = useSetRecoilState(latestPostsByUserState);
