e96373e53e1b87c9f6d1f63201baa57aa4db8528
Kevin Pang <moistbobo@gmail.com>
2023-01-06 21:56:38 +0900

    refactor(PostComponent): more efficient useMemo dependency

--- src/components/PostComponent/index.tsx
+++ src/components/PostComponent/index.tsx
@@ -46,7 +46,7 @@ const PostComponent = ({postData}: Props) => {
         </View>
       );
     }
-  }, [postData]);
+  }, [JSON.stringify(postData)]);
 
   return (
     <View onStartShouldSetResponder={() => true} style={styles.container}>
