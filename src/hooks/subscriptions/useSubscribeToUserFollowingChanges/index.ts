eeba14285fa8e9ffc180c7613a3ee95638c982bc
Kevin Pang <moistbobo@gmail.com>
2023-01-16 13:49:07 +0800

    fix: subscription cache preventing effects from firing

--- src/hooks/subscriptions/useSubscribeToUserFollowingChanges/index.ts
+++ src/hooks/subscriptions/useSubscribeToUserFollowingChanges/index.ts
@@ -13,6 +13,8 @@ const useSubscribeToUserFollowingChanges = () => {
     variables: {
       address: activeAddress,
     },
+    // no-cache is required as caching the result causes it to never update
+    fetchPolicy: 'no-cache',
   });
 
   const {resolveOptimisticRelationships} = useOptimisticRelationships();
@@ -34,7 +36,6 @@ const useSubscribeToUserFollowingChanges = () => {
       followingCount &&
       JSON.stringify(counterPartyArr) !== storedFollowing.current
     ) {
-      console.log('following changed');
       updateFollowing().then(() => {
         // update existing counter, make API call to update user's following list
         storedFollowing.current = JSON.stringify(counterPartyArr);
