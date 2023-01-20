5f8ed9e433e0201678a97030b0642fd5ab2d4734
Alessandro Mazzon <alessandro@forbole.com>
2023-01-20 14:13:06 +0100

    fix(DFP-947): Guest profile UI updates (#297)
    ## Guest profile UI updates
    
    Closes: DFP-947
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

--- src/screens/Profile/components/BalanceSection/useStyles.ts
+++ src/screens/Profile/components/BalanceSection/useStyles.ts
@@ -1,11 +1,9 @@
 import {makeStyle} from 'config/theme';
-import {verticalScale} from 'react-native-size-matters';
 
 const useStyles = makeStyle(theme => ({
   container: {
     flex: 1,
     paddingVertical: theme.spacing.m,
-    height: verticalScale(140),
   },
   divider: {
     backgroundColor: theme.colors.surfaceGrey,
