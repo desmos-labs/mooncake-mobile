b54ede01b430c82b9683ed5971321598f1907ee1
Alessandro Mazzon <alessandro@forbole.com>
2023-01-24 10:40:02 +0100

    chore: fix inverted try-catch function

--- src/hooks/useRefreshSession/index.ts
+++ src/hooks/useRefreshSession/index.ts
@@ -17,15 +17,6 @@ const useRefreshSession = () => {
   const refreshSession = useCallback(async () => {
     const bearerToken = getMMKV(MMKVKEYS.REST_AUTH_TOKEN);
 
-    // Notifications token refresh
-    try {
-      console.log('Obtaining notifications token');
-      const notificationsToken = await messaging().getToken();
-      await PostNotificationToken(notificationsToken.toString());
-    } catch (err: any) {
-      console.error('NOTIFICATIONS TOKEN', err);
-    }
-
     // Bearer token refresh
     try {
       if (bearerToken) {
@@ -41,6 +32,16 @@ const useRefreshSession = () => {
     } catch (err: any) {
       console.error('BEARER TOKEN', err);
       replace(ROUTES.LOGIN);
+    }
+
+    // Notifications token refresh
+    try {
+      console.log('Obtaining notifications token');
+      const notificationsToken = await messaging().getToken();
+      await PostNotificationToken(notificationsToken.toString());
+      console.log('Posted notifications token');
+    } catch (err: any) {
+      console.error('NOTIFICATIONS TOKEN', err);
     }
   }, []);
 
