7581cd5ffafce3cb8e5a834e42588321e9126c53
Kevin Pang <moistbobo@gmail.com>
2023-02-02 13:57:25 +0800

    test: Add simple test for onboarding

--- e2e/release/onboarding.release.test.js
+++ e2e/release/onboarding.release.test.js
@@ -12,4 +12,16 @@ describe('App first launch should display welcome screen.', () => {
   it('Welcome screen should be shown', async () => {
     await expect(element(by.text('Welcome to Butter'))).toBeVisible();
   });
+
+  it('Goes to landing page if the skip button is pressed', async () => {
+    await element(by.text('Skip')).tap();
+    await expect(element(by.text('Butter'))).toBeVisible();
+    await expect(
+      element(by.text('Your decentralized social network')),
+    ).toBeVisible();
+    await expect(element(by.text('Sign Up'))).toBeVisible();
+    await expect(
+      element(by.text('Import Secret Recovery Phrase')),
+    ).toBeVisible();
+  });
 });
