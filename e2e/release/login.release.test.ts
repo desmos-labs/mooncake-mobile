b5973b805b5050665e89fd09b5bb8b3fc49a7ead
Alessandro Mazzon <alessandro@forbole.com>
2023-02-03 14:29:48 +0100

    detox: exclude e2e from linting

--- e2e/release/login.release.test.ts
+++ e2e/release/login.release.test.ts
@@ -3,7 +3,7 @@ import {
   DETOX_DEV_ACCOUNT_NICKNAME,
   DETOX_DEV_BLANK_MNEMONIC,
   DETOX_DEV_MNEMONIC,
-} from '../__mocks__/detox.env.sample';
+} from '../__mocks__/detox.env';
 import launchAppConfig from '../config';
 
 describe('Login flow', () => {
