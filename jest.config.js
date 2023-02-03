ca581ef596f31673f03f49baeb5dd5b226600e87
Kevin Pang <moistbobo@gmail.com>
2023-02-03 13:09:41 +0800

    chore: move jest config to external jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const config = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-native|@react-navigation)|crypto-es',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    '<rootDir>/jest/setupAfterEnv.js',
+    '<rootDir>/jest/globalMock.js',
+  ],
+  setupFiles: [
+    '<rootDir>/jest/setup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/assets/'],
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
+};
+
+module.exports = config;
