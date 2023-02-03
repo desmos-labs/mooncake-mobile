3606a49828fdebf00b6e70c7d4207e83a8caa712
Kevin Pang <moistbobo@gmail.com>
2023-02-03 15:53:02 +0800

    chore(detox): Add test environment detector (#311)

--- e2e/jest.config.base.js
+++ e2e/jest.config.base.js
@@ -1,7 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   rootDir: '..',
-  testMatch: ['<rootDir>/e2e/**/*.test.ts'],
   testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
