4f964eba4af353212336a66715d12b14a3f38c5c
Kevin Pang <moistbobo@gmail.com>
2023-02-04 16:21:20 +0800

    chore: restore reassure scripts

--- reassure-tests.sh
+++ reassure-tests.sh
@@ -7,11 +7,11 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 git fetch origin
 
 # Gather baseline perf measurements
-#git switch "$BASELINE_BRANCH"
-#yarn install --force
-#yarn reassure --baseline
+git switch "$BASELINE_BRANCH"
+yarn install --force
+yarn reassure --baseline
 
 # Gather current perf measurements & compare results
-#git switch --detach -
+git switch --detach -
 yarn install --force
 yarn reassure
