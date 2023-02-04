fcc0e0d25a51bfb3614732ba83fb7cffab39da57
Kevin Pang <moistbobo@gmail.com>
2023-02-04 15:54:20 +0800

    chore: temporarily disable baseline measurement

--- reassure-tests.sh
+++ reassure-tests.sh
@@ -8,8 +8,8 @@ git fetch origin
 
 # Gather baseline perf measurements
 #git switch "$BASELINE_BRANCH"
-yarn install --force
-yarn reassure --baseline
+#yarn install --force
+#yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git switch --detach -
