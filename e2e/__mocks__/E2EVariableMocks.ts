38696fd2a1144b3ddcc7b1bdf232b179c043fb09
Alessandro Mazzon <alessandro@forbole.com>
2023-02-06 16:39:24 +0100

    chore: fix dotenv path

--- e2e/__mocks__/E2EVariableMocks.ts
+++ e2e/__mocks__/E2EVariableMocks.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
 
 // Load private variable mocks from e2e/.env
-dotenv.config({path: '../.env'});
+dotenv.config({path: './e2e/.env'});
 const Config = process.env;
-
+console.log(Config);
 export const DETOX_DEV_MNEMONIC = Config.DETOX_DEV_MNEMONIC || '';
 export const DETOX_DEV_ACCOUNT_NICKNAME =
   Config.DETOX_DEV_ACCOUNT_NICKNAME || '';
