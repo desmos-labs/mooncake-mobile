5fda164058841793dbc61560cbc36b5e3aec6953
Manuel Turetta <manuel.turetta94@gmail.com>
2023-02-07 15:25:27 -0500

    refactor: update active account hook

--- src/recoil/activeAccount.ts
+++ src/recoil/activeAccount.ts
@@ -1,11 +1,13 @@
-import {atom, useRecoilValue, useSetRecoilState} from 'recoil';
-import {getMMKV, MMKVKEYS, setMMKV} from 'lib/MMKVStorage';
+import { atom, selector, useRecoilValue, useSetRecoilState } from 'recoil';
+import { getMMKV, MMKVKEYS, setMMKV } from 'lib/MMKVStorage';
+import { Account } from 'types/account';
+import { accountsAppState } from '@recoil/accounts';
 
 const activeAccountAddressState = atom<string>({
   key: 'activeAccountAddress',
   default: getMMKV(MMKVKEYS.ACTIVE_ACCOUNT_ADDRESS) ?? '',
   effects: [
-    ({onSet}) => {
+    ({ onSet }) => {
       onSet(newValue => {
         setMMKV(MMKVKEYS.ACTIVE_ACCOUNT_ADDRESS, newValue);
       });
@@ -13,8 +15,20 @@ const activeAccountAddressState = atom<string>({
   ],
 });
 
-export const useSetActiveAccountAddress = () =>
-  useSetRecoilState(activeAccountAddressState);
+export const useSetActiveAccountAddress = () => useSetRecoilState(activeAccountAddressState);
 
-export const useActiveAccountAddress = () =>
-  useRecoilValue(activeAccountAddressState);
+export const useActiveAccountAddress = () => useRecoilValue(activeAccountAddressState);
+
+const activeAccountState = selector<Account | undefined>({
+  key: 'activeAccountAddress',
+  get: ({ get }) => {
+    const accounts = get(accountsAppState);
+    const selectedAccountAddress = get(activeAccountAddressState);
+    return selectedAccountAddress ? accounts[selectedAccountAddress] : undefined;
+  },
+});
+
+/**
+ * Hook that provide the current active account.
+ */
+export const useActiveAccount = () => useRecoilValue(activeAccountState);
