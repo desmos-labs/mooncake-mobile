c8b0b28b98a291a347a7b5fbb468646d2d191074
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-02-21 10:38:23 -0500

    feat: implement the hook to get the following addresses

--- src/hooks/relationships/useFollowingAddresses.ts
+++ src/hooks/relationships/useFollowingAddresses.ts
@@ -1,10 +1,12 @@
+import useFollowing from 'hooks/relationships/useFollowing';
+
 /**
  * Hook that allows to get all the addresses of the users that the current application
  * user is following.
- * TODO: Implement this
  */
 const useFollowingAddresses = () => {
-  return [] as string[];
+  const { following } = useFollowing();
+  return following.map(user => user.address);
 };
 
 export default useFollowingAddresses;
