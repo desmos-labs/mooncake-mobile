786a04d630be92637fc69ead6e8eafd8b6e726bf
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-03-14 14:33:13 -0500

    chore: improve post details hook dependencies

--- src/screens/PostDetails/index.tsx
+++ src/screens/PostDetails/index.tsx
@@ -392,7 +392,15 @@ const PostDetails = () => {
     );
   }, [
     post,
+    styles.customTopBarContainer,
+    styles.customTopBarInnerContainer,
+    styles.rightContainer,
+    styles.middleTextContainer,
+    styles.followIcon,
+    styles.moreIcon,
+    styles.topBar,
     goBack,
+    theme.spacing.m,
     formatDate,
     activeAddress,
     isFollowingAddress,
