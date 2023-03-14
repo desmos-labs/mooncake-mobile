f928bbd55e950a87366bb7a4dc4547913e78730b
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-03-14 14:37:09 -0500

    fix: fixed tips count inside post details

--- src/services/graphql/queries/GetPostTipsCount.ts
+++ src/services/graphql/queries/GetPostTipsCount.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 const GetPostTipsCount = gql`
   query GetPostTipsCount($subspaceId: bigint, $postId: bigint) @api(name: butter) {
     tips: tip_post_aggregate(
-      where: { post: { subspace_id: { _eq: $subspaceId }, id: { _eq: $postID } } }
+      where: { post: { subspace_id: { _eq: $subspaceId }, id: { _eq: $postId } } }
     ) {
       aggregate {
         count
