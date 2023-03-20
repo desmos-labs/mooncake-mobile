4a486a222813afaba5253206b961c8e5abb8f41f
Kevin Pang <moistbobo@gmail.com>
2023-03-20 12:35:51 +0800

    Revert "chore: remove ContentLoaders"
    This reverts commit f4da87a78158d37b4e6bfac65776ba8e0d2e0e17.

---
+++ src/components/Loaders/TextRowContentLoader/index.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import ContentLoader, { Rect } from 'react-content-loader/native';
+import { Dimensions } from 'react-native';
+import { useTheme } from 'native-base';
+
+const TextRowContentLoader = ({ width }: { width: string }) => {
+  const theme = useTheme();
+
+  return (
+    <ContentLoader
+      animate={true}
+      speed={2}
+      width={Dimensions.get('window').width - 32}
+      height={14}
+      backgroundColor={theme.colors.surfaceGrey}
+      foregroundColor={theme.colors.background}>
+      <Rect x="0" y="4" rx="3" ry="3" width={width} height="7" />
+    </ContentLoader>
+  );
+};
+
+export default memo(TextRowContentLoader);
