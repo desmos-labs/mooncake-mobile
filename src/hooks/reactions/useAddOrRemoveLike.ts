fa74c134af997544508d5d423558e14da91c492d
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-03-23 15:49:02 -0500

    chore: added useful comments

--- src/hooks/reactions/useAddOrRemoveLike.ts
+++ src/hooks/reactions/useAddOrRemoveLike.ts
@@ -10,13 +10,20 @@ import { debounce } from 'lodash';
 const useAddOrRemoveLike = (post: Post) => {
   const hasReacted = useHasReacted(post);
 
+  // Local state used to avoid the need to wait for the server response
+  // and have a more responsive UI as soon as the user presses the like button
   const [liked, setLiked] = React.useState(hasReacted);
+
+  // Debounce the add or remove reaction function to avoid spamming the server
   const addOrRemoveReaction = useAddOrRemoveReaction();
   const addOrRemovePostReactionDebounced = useMemo(
     () => debounce(addOrRemoveReaction, 500),
     [addOrRemoveReaction],
   );
 
+  // Function that is called when the user presses the like button
+  // This immediately updates the local state to have a more responsive UI,
+  // but then debounces the actual call to the server to avoid spamming it
   const addOrRemoveLike = useCallback(
     (p: Post) => {
       setLiked(value => !value);
