82f51dd3999b428bd337e9690c85c9fdcbdf924e
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-03-27 14:43:41 -0500

    refactor: split PostCard child components into separate files

--- src/screens/Home/components/PostCard/index.tsx
+++ src/screens/Home/components/PostCard/index.tsx
@@ -1,35 +1,11 @@
-import { loadingOrange } from 'assets/animations';
-import {
-  followBlackIcon,
-  postLikedIcon,
-  postToCommentIcon,
-  postToLikeIcon,
-  postToTipIcon,
-  reportIcon,
-  unfollowBlackIcon,
-} from 'assets/images';
-import ImageButton from 'components/ImageButton';
-import PopupMenu from 'components/PopupMenu';
-import ThemedLottieView from 'components/ThemedLottieView';
 import Typography from 'components/Typography';
-import { parseISO } from 'date-fns';
 import useRenderMediaAttachment from 'hooks/rendering/useRenderMediaAttachment';
-import useFormatTimeForPostDetails from 'hooks/formatting/useFormatTimeForPostDetails';
-import { formatMsToHumanReadable } from 'lib/FormatUtils';
-import React, { memo, useCallback, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { memo, useMemo } from 'react';
 import { TouchableOpacity, View } from 'react-native';
-import FastImage from 'react-native-fast-image';
-import { VStack, HStack, Center, useTheme } from 'native-base';
+import { useTheme } from 'native-base';
 import { isPostPending, Post } from 'types/posts';
-import useIsFollowing from 'hooks/relationships/useIsFollowing';
-import { useActiveAccountAddress } from '@recoil/accounts';
-import usePostReactionsCount from 'hooks/reactions/usePostReactionsCount';
-import usePostCommentsCount from 'hooks/posts/comments/usePostCommentsCount';
-import { getProfilePicture } from 'lib/ProfileUtils';
-import useAddOrRemoveLike from 'hooks/reactions/useAddOrRemoveLike';
-import useCustomToast from 'hooks/extended/useCustomToast';
-import CommonStyles from 'config/theme/CommonStyles';
+import PostCardBottomBar from 'screens/Home/components/PostCardBottomBar';
+import PostCardProfileInfo from 'screens/Home/components/PostCardProfileInfo';
 import useStyles from './useStyles';
 
 interface PostCardProps {
@@ -73,9 +49,7 @@ interface PostCardProps {
 const PostCard = (props: PostCardProps) => {
   const styles = useStyles();
   const theme = useTheme();
-  const { t } = useTranslation('home');
-  const toast = useCustomToast();
-  // Unwrap the props
+
   const {
     post,
     onPressAuthor,
@@ -87,99 +61,10 @@ const PostCard = (props: PostCardProps) => {
   } = props;
 
   // -------------------------------------------------------------------------------------
-  // --- Utility hooks
+  // --- Memoized variables
   // -------------------------------------------------------------------------------------
-
-  const activeAddress = useActiveAccountAddress();
-  const isFollowing = useIsFollowing(post.author.address);
-  const { count: reactionsCount } = usePostReactionsCount(post);
-  const { count: commentsCount } = usePostCommentsCount(post);
-
-  // -------------------------------------------------------------------------------------
-  // --- Popup Menu Items
-  // -------------------------------------------------------------------------------------
-
-  const popupMenuItems = React.useMemo(
-    () => [
-      {
-        label: isFollowing ? t('unfollow') : t('follow'),
-        onPress: onPressFollow,
-        icon: isFollowing ? unfollowBlackIcon : followBlackIcon,
-      },
-      {
-        label: t('report'),
-        onPress: onPressReport,
-        icon: reportIcon,
-      },
-    ],
-    [isFollowing, t, onPressFollow, onPressReport],
-  );
-
-  // -------------------------------------------------------------------------------------
-  // --- Formatted data
-  // -------------------------------------------------------------------------------------
-
-  const isCurrentUserAuthor = useMemo(
-    () => post.author.address === activeAddress,
-    [post, activeAddress],
-  );
 
   const isPending = useMemo(() => isPostPending(post), [post]);
-
-  const formatDate = useFormatTimeForPostDetails();
-  const formattedDate = formatDate(post.creationDate);
-  const calculatedCreationDate = useMemo(() => {
-    const parsedTime = parseISO(`${post.creationDate}Z`);
-    const now = new Date();
-
-    const differenceInUnix = now.getTime() - parsedTime.getTime();
-    if (differenceInUnix < 59999) {
-      return t('seconds ago', {
-        count: formatMsToHumanReadable(differenceInUnix, 'seconds'),
-      });
-    } else if (differenceInUnix < 3599999) {
-      return t('minutes ago', {
-        count: formatMsToHumanReadable(differenceInUnix, 'minutes'),
-      });
-    } else if (differenceInUnix < 86399999) {
-      return t('hours ago', {
-        count: formatMsToHumanReadable(differenceInUnix, 'hours'),
-      });
-    } else if (differenceInUnix < 31556951999) {
-      return t('days ago', {
-        count: formatMsToHumanReadable(differenceInUnix, 'days'),
-      });
-    } else {
-      return formattedDate;
-    }
-  }, [formattedDate, post.creationDate, t]);
-
-  // -------------------------------------------------------------------------------------
-  // --- Utility functions
-  // -------------------------------------------------------------------------------------
-
-  /**
-   * Checks if the current user is the author of the post and shows a toast if that's the case or calls the handler to send tips
-   */
-  const checkUserAndHandleSendTips = useCallback(() => {
-    if (isCurrentUserAuthor) {
-      toast.errorNoRetry(t('common:cannot tip yourself'));
-    } else {
-      onPressTip();
-    }
-  }, [onPressTip, isCurrentUserAuthor, t, toast]);
-
-  // -------------------------------------------------------------------------------------
-  // --- Handles
-  // -------------------------------------------------------------------------------------
-
-  const { liked, addOrRemoveLike } = useAddOrRemoveLike(post);
-  const onPressLike = useCallback(async () => {
-    if (isPostPending(post)) {
-      return toast.success(t('toast:postTxInProgress'));
-    }
-    addOrRemoveLike(post);
-  }, [addOrRemoveLike, post, t, toast]);
 
   // -------------------------------------------------------------------------------------
   // --- Child components
@@ -192,118 +77,28 @@ const PostCard = (props: PostCardProps) => {
     resizeMode: 'contain',
   });
 
-  const PendingIndicator = useMemo(() => {
-    if (isPending) {
-      return <ThemedLottieView source={loadingOrange} autoPlay style={styles.pendingIcon} />;
-    } else if (!isCurrentUserAuthor) {
-      return <PopupMenu menuItems={popupMenuItems} />;
-    }
-  }, [popupMenuItems, isPending, isCurrentUserAuthor, styles.pendingIcon]);
-
-  const ProfileInfo = React.useMemo(() => {
-    return (
-      <View style={styles.profileInfoView}>
-        <TouchableOpacity style={CommonStyles.flexDirection.row} onPress={onPressAuthor}>
-          <FastImage source={getProfilePicture(post.author)} style={styles.profilePic} />
-          <VStack>
-            <Typography.Subtitle2>{post.author.nickname}</Typography.Subtitle2>
-            <HStack>
-              <Typography.Body6 style={{ color: theme.colors.midGrey }}>
-                @{post.author.dTag}
-              </Typography.Body6>
-              <Typography.Body6
-                style={{
-                  color: theme.colors.midGrey,
-                  marginLeft: theme.spacing.xs,
-                }}>
-                {!isPending && `· ${calculatedCreationDate}`}
-              </Typography.Body6>
-            </HStack>
-          </VStack>
-        </TouchableOpacity>
-        <Center justifyContent="flex-start">{PendingIndicator}</Center>
-      </View>
-    );
-  }, [
-    styles.profileInfoView,
-    styles.profilePic,
-    onPressAuthor,
-    post.author,
-    theme.colors.midGrey,
-    theme.spacing.xs,
-    isPending,
-    calculatedCreationDate,
-    PendingIndicator,
-  ]);
-
-  const BottomBar = React.useMemo(() => {
-    return (
-      <View style={styles.bottomBarView}>
-        <View style={styles.bottomBarInnerView}>
-          <ImageButton
-            onPress={onPressLike}
-            tintColor={liked ? theme.colors.butterOrange01 : theme.colors.grey02}
-            image={liked ? postLikedIcon : postToLikeIcon}
-            style={styles.bottomBarIcon}
-          />
-          <Typography.Subtitle3
-            style={liked ? { color: theme.colors.butterOrange01 } : { color: theme.colors.grey02 }}>
-            {reactionsCount}
-          </Typography.Subtitle3>
-          {/* I have completely removed the logic that changed the color of the button based on whether */}
-          {/* the user comment the post or not. This has been done for the following reasons: */}
-          {/* 1. It's a bad UX: no social network changes the color of the buttons for this reason */}
-          {/* 2. It's extremely hard to implement, and completely useless in the first place */}
-          <TouchableOpacity onPress={onPressComment} style={styles.commentButton}>
-            <FastImage
-              resizeMode="cover"
-              tintColor={theme.colors.grey02}
-              source={postToCommentIcon}
-              style={styles.bottomBarIcon}
-            />
-            <Typography.Subtitle3 style={{ color: theme.colors.grey02 }}>
-              {commentsCount}
-            </Typography.Subtitle3>
-          </TouchableOpacity>
-        </View>
-
-        {/* I have completely removed the logic that changed the color of the button based on whether */}
-        {/* the user tipped the post or not. This has been done for the following reasons: */}
-        {/* 1. It's a bad UX: no social network changes the color of the buttons for this reason */}
-        {/* 2. It's extremely hard to implement, and completely useless in the first place */}
-        <TouchableOpacity onPress={checkUserAndHandleSendTips} style={styles.tipButton}>
-          <FastImage resizeMode="cover" source={postToTipIcon} style={styles.bottomBarIcon} />
-          <Typography.Subtitle3 style={{ color: theme.colors.grey02 }}>
-            {t('tip')}
-          </Typography.Subtitle3>
-        </TouchableOpacity>
-      </View>
-    );
-  }, [
-    styles.bottomBarView,
-    styles.bottomBarInnerView,
-    styles.bottomBarIcon,
-    styles.commentButton,
-    onPressLike,
-    liked,
-    theme.colors.butterOrange01,
-    theme.colors.grey02,
-    theme.spacing.s,
-    reactionsCount,
-    onPressComment,
-    commentsCount,
-    checkUserAndHandleSendTips,
-    t,
-  ]);
-
   return (
     <TouchableOpacity activeOpacity={0.9} style={styles.container} onPress={onPressDetails}>
-      {ProfileInfo}
+      {/* Post author info */}
+      <PostCardProfileInfo
+        post={post}
+        onPressAuthor={onPressAuthor}
+        onPressFollow={onPressFollow}
+        onPressReport={onPressReport}
+      />
+
+      {/* Post text */}
       {post.text && (
         <Typography.Body6 style={{ marginTop: theme.spacing.m }}>{post.text}</Typography.Body6>
       )}
+
+      {/* Media view */}
       {MediaAttachment && <View style={styles.mediaView}>{MediaAttachment}</View>}
-      {!isPending && BottomBar}
+
+      {/* Post bottom bar */}
+      {!isPending && (
+        <PostCardBottomBar post={post} onPressComment={onPressComment} onPressTip={onPressTip} />
+      )}
     </TouchableOpacity>
   );
 };
---
+++ src/screens/Home/components/PostCardBottomBar/index.tsx
@@ -0,0 +1,124 @@
+import { TouchableOpacity, View } from 'react-native';
+import ImageButton from 'components/ImageButton';
+import { postLikedIcon, postToCommentIcon, postToLikeIcon, postToTipIcon } from 'assets/images';
+import Typography from 'components/Typography';
+import FastImage from 'react-native-fast-image';
+import React, { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useTheme } from 'native-base';
+import usePostReactionsCount from 'hooks/reactions/usePostReactionsCount';
+import usePostCommentsCount from 'hooks/posts/comments/usePostCommentsCount';
+import { isPostPending, Post } from 'types/posts';
+import useAddOrRemoveLike from 'hooks/reactions/useAddOrRemoveLike';
+import useCustomToast from 'hooks/extended/useCustomToast';
+import { useActiveAccountAddress } from '@recoil/accounts';
+import useStyles from './useStyles';
+
+export interface PostBottomBarProps {
+  readonly post: Post;
+  readonly onPressComment: () => void;
+  readonly onPressTip: () => void;
+}
+
+/**
+ * Bottom bar that contains the buttons to interact with a post.
+ * @constructor
+ */
+const PostCardBottomBar = (props: PostBottomBarProps) => {
+  const { t } = useTranslation('home');
+  const styles = useStyles();
+  const theme = useTheme();
+  const toast = useCustomToast();
+
+  const { post, onPressComment, onPressTip } = props;
+
+  // -------------------------------------------------------------------------------------
+  // --- Utility hooks
+  // -------------------------------------------------------------------------------------
+
+  const activeAddress = useActiveAccountAddress();
+  const { count: reactionsCount } = usePostReactionsCount(post);
+  const { count: commentsCount } = usePostCommentsCount(post);
+
+  const { liked, addOrRemoveLike } = useAddOrRemoveLike(post);
+
+  // -------------------------------------------------------------------------------------
+  // --- Memoized variables
+  // -------------------------------------------------------------------------------------
+
+  const isCurrentUserAuthor = useMemo(
+    () => post.author.address === activeAddress,
+    [post, activeAddress],
+  );
+
+  // -------------------------------------------------------------------------------------
+  // --- Callbacks
+  // -------------------------------------------------------------------------------------
+
+  const onPressLike = useCallback(async () => {
+    if (isPostPending(post)) {
+      return toast.success(t('toast:postTxInProgress'));
+    }
+    addOrRemoveLike(post);
+  }, [addOrRemoveLike, post, t, toast]);
+
+  /**
+   * Checks if the current user is the author of the post and shows a toast if that's the case or calls the handler to send tips
+   */
+  const checkUserAndHandleSendTips = useCallback(() => {
+    if (isCurrentUserAuthor) {
+      toast.errorNoRetry(t('common:cannot tip yourself'));
+    } else {
+      onPressTip();
+    }
+  }, [onPressTip, isCurrentUserAuthor, t, toast]);
+
+  // -------------------------------------------------------------------------------------
+  // --- View rendering
+  // -------------------------------------------------------------------------------------
+
+  return (
+    <View style={styles.bottomBarView}>
+      <View style={styles.bottomBarInnerView}>
+        <ImageButton
+          onPress={onPressLike}
+          tintColor={liked ? theme.colors.butterOrange01 : theme.colors.grey02}
+          image={liked ? postLikedIcon : postToLikeIcon}
+          style={styles.bottomBarIcon}
+        />
+        <Typography.Subtitle3
+          style={liked ? { color: theme.colors.butterOrange01 } : { color: theme.colors.grey02 }}>
+          {reactionsCount}
+        </Typography.Subtitle3>
+        {/* I have completely removed the logic that changed the color of the button based on whether */}
+        {/* the user comment the post or not. This has been done for the following reasons: */}
+        {/* 1. It's a bad UX: no social network changes the color of the buttons for this reason */}
+        {/* 2. It's extremely hard to implement, and completely useless in the first place */}
+        <TouchableOpacity onPress={onPressComment} style={styles.commentButton}>
+          <FastImage
+            resizeMode="cover"
+            tintColor={theme.colors.grey02}
+            source={postToCommentIcon}
+            style={styles.bottomBarIcon}
+          />
+          <Typography.Subtitle3 style={{ color: theme.colors.grey02 }}>
+            {commentsCount}
+          </Typography.Subtitle3>
+        </TouchableOpacity>
+      </View>
+
+      {/* I have completely removed the logic that changed the color of the button based on whether */}
+      {/* the user tipped the post or not. This has been done for the following reasons: */}
+      {/* 1. It's a bad UX: no social network changes the color of the buttons for this reason */}
+      {/* 2. It's extremely hard to implement, and completely useless in the first place */}
+      <TouchableOpacity onPress={checkUserAndHandleSendTips} style={styles.tipButton}>
+        <FastImage resizeMode="cover" source={postToTipIcon} style={styles.bottomBarIcon} />
+        <Typography.Subtitle3 style={{ color: theme.colors.grey02 }}>
+          {t('tip')}
+        </Typography.Subtitle3>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default PostCardBottomBar;
