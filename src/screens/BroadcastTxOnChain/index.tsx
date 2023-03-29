7de91c20121dcf836f1be34355c649db822be9f1
Kevin Pang <moistbobo@gmail.com>
2023-03-29 15:15:57 +0800

    wip: BroadcastTxOnChain UI

--- src/screens/BroadcastTxOnChain/index.tsx
+++ src/screens/BroadcastTxOnChain/index.tsx
@@ -6,21 +6,36 @@ import Typography from 'components/Typography';
 import { RootNavigatorParamList } from 'navigation/RootNavigator';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import ROUTES from 'navigation/routes';
 import { useRoute } from '@react-navigation/native';
 import { broadcastAnim } from 'assets/animations';
-import { EncodeObject } from '@desmoslabs/desmjs';
+import {
+  EncodeObject,
+  MsgAddReactionTypeUrl,
+  MsgCreateRelationshipTypeUrl,
+  MsgCreateReportTypeUrl,
+  MsgDeleteRelationshipTypeUrl,
+  MsgGrantAllowanceTypeUrl,
+  MsgGrantTypeUrl,
+  MsgRemoveReactionTypeUrl,
+  MsgRevokeAllowanceTypeUrl,
+} from '@desmoslabs/desmjs';
 import useOnBackAction from 'hooks/navigation/useOnBackAction';
 import { Result } from 'neverthrow';
 import { StdFee } from '@cosmjs/amino';
 import Button from 'components/Button';
 import { isCanceledOperationError } from 'types/error';
 import { Wallet } from 'types/wallet';
-import { useTheme } from 'native-base';
+import { Box, useTheme } from 'native-base';
 import useCustomToast from 'hooks/extended/useCustomToast';
 import { PendingTransaction } from 'types/transactions';
 import useEstimateTransactionFees from 'hooks/transactions/useEstimateTransactionFees';
+import TransactionRow from 'screens/BroadcastTxOnChain/components/TransactionRow';
+import { formatCoins } from 'lib/FormatUtils';
+import _ from 'lodash';
+import TopBar from 'components/TopBar';
+import { MsgExecuteContractTypeUrl } from 'config/AutzGrants';
 import useBroadcastTx from './useBroadcastTx';
 import useStyles from './useStyles';
 
@@ -113,6 +128,74 @@ const BroadcastTxOnChain: React.FC = () => {
   }, []);
 
   // -----------------------------------------------------------------------
+  // --- Memoized values
+  // -----------------------------------------------------------------------
+
+  const broadcasterAddress: string = React.useMemo(() => {
+    return (accountAddressOrWallet as Wallet).address || (accountAddressOrWallet as string);
+  }, [accountAddressOrWallet]);
+
+  const transactionType: string = React.useMemo(() => {
+    const msgTypes = messages.map(x => x.typeUrl);
+
+    // turn the msgType array into an object for faster indexing.
+    const msgTypeObj: { [index: string]: number } = msgTypes.reduce((acc, cur) => {
+      return {
+        ...acc,
+        [cur]: 1,
+      };
+    }, {});
+
+    // Authorization
+    if (
+      msgTypeObj[MsgRevokeAllowanceTypeUrl] ||
+      msgTypeObj[MsgGrantAllowanceTypeUrl] ||
+      msgTypeObj[MsgGrantTypeUrl]
+    ) {
+      return t('broadcastTxOnChain:authorization');
+    }
+    // like post
+    if (msgTypeObj[MsgAddReactionTypeUrl]) {
+      return t('broadcastTxOnChain:likePost');
+    }
+    // dislike post
+    if (msgTypeObj[MsgRemoveReactionTypeUrl]) {
+      return t('broadcastTxOnChain:dislikePost');
+    }
+    // smart contracts
+    if (msgTypeObj[MsgExecuteContractTypeUrl]) {
+      // tipping a post
+      // TODO: figure out how to differentiate between smart contracts
+      return t('broadcastTxOnChain:tipPost');
+    }
+    // follow a user
+    if (msgTypeObj[MsgCreateRelationshipTypeUrl]) {
+      return t('broadcastTxOnChain:followUser');
+    }
+    // unfollow a user
+    if (msgTypeObj[MsgDeleteRelationshipTypeUrl]) {
+      return t('broadcastTxOnChain:unfollowUser');
+    }
+    if (msgTypeObj[MsgCreateReportTypeUrl]) {
+      return t('broadcastTxOnChain:reportPost');
+    }
+
+    // unmapped, return all transaction types as a string
+    return msgTypes.join(', ');
+  }, [messages, t]);
+
+  const transactionFee = React.useMemo(() => {
+    if (!feesResult) {
+      return '';
+    }
+    if (feesResult.isOk()) {
+      return formatCoins(_.get(feesResult, 'value.amount'));
+    } else {
+      return feesResult.error.message;
+    }
+  }, [feesResult]);
+
+  // -----------------------------------------------------------------------
   // --- Actions
   // -----------------------------------------------------------------------
 
@@ -130,48 +213,37 @@ const BroadcastTxOnChain: React.FC = () => {
     }
   }, [accountAddressOrWallet, broadcastTx, feesResult, memo, messages, onSuccess, toast]);
 
+  const broadcastTxAnimation = React.useMemo(() => {
+    if (broadcastingTx) {
+      return (
+        <>
+          <ThemedLottieView autoSize autoPlay loop source={broadcastAnim} />
+          <Spacer paddingVertical={12}>
+            <Typography.H4>{title || t('transaction broadcasting')}</Typography.H4>
+          </Spacer>
+          <Typography.Body6>{t('please wait')}</Typography.Body6>
+        </>
+      );
+    }
+  }, [broadcastingTx, t, title]);
+
   // -----------------------------------------------------------------------
   // --- Screen rendering
   // -----------------------------------------------------------------------
 
   return (
-    <DView style={styles.root}>
-      <View style={styles.container}>
+    <DView topBar={<TopBar />} style={styles.root}>
+      <Box px="m" mb="50px">
+        <Typography.H3>{t('header')}</Typography.H3>
+      </Box>
+      <ScrollView contentContainerStyle={styles.scrollviewContentContainer}>
         {/* Broadcasting animation shown while the transaction it's broadcasting */}
-        {broadcastingTx && (
-          <>
-            <ThemedLottieView autoSize autoPlay loop source={broadcastAnim} />
-            <Spacer paddingVertical={12}>
-              <Typography.H4>{title || t('transaction broadcasting')}</Typography.H4>
-            </Spacer>
-            <Typography.Body6>{t('please wait')}</Typography.Body6>
-          </>
-        )}
+        {broadcastTxAnimation}
         {/* Messages list, tx fees and memo */}
-        {!broadcastingTx && (
-          <>
-            {/* TODO: Create a proper UI to display the tx messages */}
-            {/* ignored as component is temporary */}
-            {/* eslint-disable-next-line react-native/no-inline-styles */}
-            <ScrollView style={{ minHeight: '80%', flex: 1 }}>
-              <Typography.Body5>{JSON.stringify(messages)}</Typography.Body5>
-            </ScrollView>
-            {estimatingFees || feesResult === undefined ? (
-              /* TODO: Create a proper UI with a spinner or something else */
-              <Typography.Body5>Estimating fees...</Typography.Body5>
-            ) : (
-              /* TODO: Create a proper UI to render the fee result */
-              <Typography.Body5>
-                Fees:{' '}
-                {feesResult.isOk() ? JSON.stringify(feesResult.value) : feesResult.error.message}
-              </Typography.Body5>
-            )}
-            <Typography.Body5>
-              {t('memo')}: {memo ?? 'N/A'}
-            </Typography.Body5>
-          </>
-        )}
-      </View>
+        <TransactionRow title={t('address')} subtitle={broadcasterAddress} />
+        <TransactionRow title={t('type')} subtitle={transactionType} />
+        <TransactionRow title={t('fee')} isLoading={!feesResult} subtitle={transactionFee} />
+      </ScrollView>
       <Button
         size={44}
         backgroundColor={theme.colors.surfaceBlack}
--- src/screens/BroadcastTxOnChain/useStyles.ts
+++ src/screens/BroadcastTxOnChain/useStyles.ts
@@ -3,13 +3,9 @@ import { makeStyle } from 'config/theme';
 const useStyles = makeStyle(theme => ({
   root: {
     flex: 1,
-    paddingHorizontal: theme.spacing.m,
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
+  scrollviewContentContainer: {
+    paddingHorizontal: theme.spacing.m,
   },
 }));
 
