0a90d240a4aacdd0eb9f51c5c297757096dc38b4
Kevin Pang <moistbobo@gmail.com>
2023-03-30 18:53:01 +0800

    fix(DFP-1112): fix missing 'x' icon symbol inside added images when creating a post (#445)
    ## Description
    
    Closes: 
    https://forbole.atlassian.net/browse/DFP-1112
    #413 
    
    This PR fixes the missing x icon for images when selecting an image for
    a post.
    
    It also fixes a minor issue where the selected post image would get
    pushed down to an unscrollable area when there is lots of text. (See
    attachment for before).
    
    
    
    https://user-images.githubusercontent.com/29080587/228756784-b71ece03-b23f-448d-9122-2619dc214bd3.mp4
    
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

--- src/screens/CreatePost/index.tsx
+++ src/screens/CreatePost/index.tsx
@@ -12,7 +12,7 @@ import { RootNavigatorParamList } from 'navigation/RootNavigator';
 import ROUTES from 'navigation/routes';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import MediaBottomPanel from 'components/MediaBottomPanel';
-import { Center, useTheme } from 'native-base';
+import { useTheme } from 'native-base';
 import { Post } from 'types/posts';
 import useCreatePost from 'hooks/posts/useCreatePost';
 import {
@@ -191,16 +191,14 @@ const CreatePost = () => {
               textAlignVertical="top"
             />
             {/* TODO: Allow to select multiple attachments */}
-            <Center>
-              <SelectedPostImage
-                source={postAttachments.length > 0 ? { uri: postAttachments[0].uri } : ('' as any)}
-                dimensions={{
-                  width: postAttachments.length > 0 ? postAttachments[0].width : undefined,
-                  height: postAttachments.length > 0 ? postAttachments[0].height : undefined,
-                }}
-                handlePress={source => removePostAttachment(source)}
-              />
-            </Center>
+            <SelectedPostImage
+              source={postAttachments.length > 0 ? { uri: postAttachments[0].uri } : ('' as any)}
+              dimensions={{
+                width: postAttachments.length > 0 ? postAttachments[0].width : undefined,
+                height: postAttachments.length > 0 ? postAttachments[0].height : undefined,
+              }}
+              handlePress={source => removePostAttachment(source)}
+            />
           </ScrollView>
         </View>
       </DView>
