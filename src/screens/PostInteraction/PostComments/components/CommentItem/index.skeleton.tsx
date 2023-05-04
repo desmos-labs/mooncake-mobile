560acee2a4aa5edd9d176f4fa993e5ca397eba13
Kevin Pang <moistbobo@gmail.com>
2023-05-04 21:33:08 +0800

    feat: add skeleton post details skeleton loader (#517)
    ## Description
    
    Closes: [DFP-1218](https://forbole.atlassian.net/browse/DFP-1218)
    
    This PR adds skeleton loaders to the PostDetails screen.
    
    Here is the final effect (with and without comments):
    
    
    
    
    https://user-images.githubusercontent.com/29080587/236159105-21378d79-b821-475d-bbab-093b86c3fa91.mp4
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed
    
    
    [DFP-1218]:
    https://forbole.atlassian.net/browse/DFP-1218?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/screens/PostInteraction/PostComments/components/CommentItem/index.skeleton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HStack, Skeleton, VStack } from 'native-base';
+import useStyles from './useStyles';
+
+const CommentItemSkeleton = () => {
+  const styles = useStyles({ comment: {} as any, disableInnerComment: false });
+
+  return (
+    <HStack style={styles.container}>
+      <Skeleton style={styles.avatar} />
+      <VStack flex={1}>
+        <Skeleton mb="s" width="75%" height={3} borderRadius={12} />
+        <Skeleton width="75%" height={3} borderRadius={12} />
+        <Skeleton my="m" height={12} borderRadius={12} />
+        <Skeleton height={3} borderRadius={12} />
+      </VStack>
+    </HStack>
+  );
+};
+
+export default CommentItemSkeleton;
