e2fa26a5aa4139b5ed4e978e30e03f2c8ec99e36
Kevin Pang <moistbobo@gmail.com>
2023-05-11 01:37:43 +0800

    feat: hide post integration

--- src/hooks/posts/useHidePost.ts
+++ src/hooks/posts/useHidePost.ts
@@ -3,28 +3,33 @@ import { useActiveAccountAddress } from '@recoil/accounts';
 import HidePost from 'services/axios/requests/HidePost';
 import useCustomToast from 'hooks/extended/useCustomToast';
 import { useTranslation } from 'react-i18next';
+import { useStorePosts } from '@recoil/posts';
 
 const useHidePost = () => {
   const activeAccountAddress = useActiveAccountAddress();
+
+  if (!activeAccountAddress) {
+    throw new Error('Trying to hide a post, without an active account');
+  }
+
   const toast = useCustomToast();
   const { t } = useTranslation('toast');
+  const storePosts = useStorePosts(activeAccountAddress);
 
   return React.useCallback(
     async (postID: number) => {
-      if (!activeAccountAddress) {
-        throw new Error('Trying to hide a post, without an active account');
-      }
-
       const hidePostResult = await HidePost(postID);
 
       if (hidePostResult.isErr()) {
         toast.errorNoRetry(t('errorHidePost'));
       }
       if (hidePostResult.isOk()) {
+        // Remove the hidden post from stored posts
+        storePosts(storedPosts => storedPosts.filter(post => post.id !== postID));
         toast.success(t('postHidden'));
       }
     },
-    [activeAccountAddress, t, toast],
+    [storePosts, t, toast],
   );
 };
 
