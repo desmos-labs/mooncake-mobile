0443ab2b7174d57ca0f8d80cc128af81bdf8cc1c
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-12-05 11:09:57 -0500

    fix: fixed PostHog user tracking

--- src/hooks/analytics/useTrackUser.ts
+++ src/hooks/analytics/useTrackUser.ts
@@ -2,7 +2,7 @@ import { usePostHog } from 'posthog-react-native';
 import React from 'react';
 import useIsTestnetEvent from 'hooks/analytics/useIsTestnetEvent';
 import { identifyPostHogUser } from 'lib/PostHog/utils';
-import { useGetCurrentChainInfo } from '@recoil/settings';
+import { useCurrentChainInfo } from '@recoil/settings';
 
 /**
  * Hook that provides a function to track the user on PostHog.
@@ -10,17 +10,17 @@ import { useGetCurrentChainInfo } from '@recoil/settings';
 const useTrackUser = () => {
   const postHog = usePostHog();
   const isTestnetEvent = useIsTestnetEvent();
-  const getChainInfo = useGetCurrentChainInfo();
+  const chainInfo = useCurrentChainInfo();
 
   return React.useCallback(
     async (address: string) => {
-      if (!postHog || isTestnetEvent) {
+      if (!postHog || isTestnetEvent || !chainInfo) {
         return;
       }
 
-      await identifyPostHogUser(postHog, address, getChainInfo());
+      await identifyPostHogUser(postHog, address, chainInfo);
     },
-    [postHog, isTestnetEvent, getChainInfo],
+    [postHog, isTestnetEvent, chainInfo],
   );
 };
 
