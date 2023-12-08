41720ef4f888558473c83afa511c23ac93696483
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-12-08 13:59:16 -0500

    fix: fixed ts-prune warning on useAppFeatureFlags

--- src/hooks/featureflags/useAppFeatureFlags.ts
+++ src/hooks/featureflags/useAppFeatureFlags.ts
@@ -16,4 +16,7 @@ const useAppFeatureFlags = () => {
   return appFeatureFlags;
 };
 
+// For the time being, we disable this warning as we will surely use these function once the Landing page
+// UI has been reviewed and we need to show only the private key login for the App Store review to pass
+// ts-prune-ignore-next
 export default useAppFeatureFlags;
