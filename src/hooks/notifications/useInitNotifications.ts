9373c37af1294a66244f4a7ffcbc4cfead03199b
Manuel <manuel.turetta94@gmail.com>
2023-12-19 18:17:50 +0100

    feat: prevent handling of notifications with same id

--- src/hooks/notifications/useInitNotifications.ts
+++ src/hooks/notifications/useInitNotifications.ts
@@ -3,7 +3,7 @@ import { AppState } from 'react-native';
 import { firebase } from '@react-native-firebase/messaging';
 import useParseNotificationAndNavigate from './useParseNotificationAndNavigate';
 
-let notificationSubscription: () => void | undefined;
+let backgroundNotification: string | undefined;
 
 const useInitNotificationsLogic = () => {
   const navigateToCorrectScreen = useParseNotificationAndNavigate();
@@ -22,27 +22,22 @@ const useInitNotificationsLogic = () => {
   // Effect to handle the notification that the user has pressed while the application
   // was in background state.
   React.useEffect(() => {
-    return firebase.messaging().onNotificationOpenedApp(m => navigateToCorrectScreen(m, false));
+    return firebase.messaging().onNotificationOpenedApp(m => {
+      backgroundNotification = m.data?.notification_id;
+      navigateToCorrectScreen(m, false);
+    });
   }, [navigateToCorrectScreen]);
 
   // Effect to handle the notifications received while the application is opened.
   React.useEffect(() => {
-    const subscription = AppState.addEventListener('change', () => {
-      console.log('[Notifications] AppState change', AppState.currentState);
-      if (AppState.currentState === 'active') {
-        notificationSubscription = firebase.messaging().onMessage(m => {
-          navigateToCorrectScreen(m, true);
-        });
-      } else if (AppState.currentState === 'background') {
-        if (notificationSubscription) {
-          notificationSubscription();
-          notificationSubscription = undefined;
-        }
+    return firebase.messaging().onMessage(m => {
+      const notificationId = m.data?.notification_id;
+      const sameNotification =
+        backgroundNotification !== undefined && backgroundNotification === notificationId;
+      if (AppState.currentState === 'active' && !sameNotification) {
+        navigateToCorrectScreen(m, true);
       }
     });
-    return () => {
-      subscription.remove();
-    };
   }, [navigateToCorrectScreen]);
 };
 
