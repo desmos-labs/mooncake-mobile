b82fb6566ffd6bd995755e2f599dff54aeb346ab
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-01-08 10:15:46 -0500

    feat: use translation strings for picture editors titles

--- src/assets/locales/en/index.ts
+++ src/assets/locales/en/index.ts
@@ -1,28 +1,29 @@
-import activities from './activities.json';
-import broadcastTx from './broadcastTx.json';
-import common from './common.json';
-import createPost from './createPost.json';
-import createProfile from './createProfile.json';
-import error from './errors.json';
-import forgotPassword from './forgotPassword.json';
-import home from './home.json';
-import landing from './landing.json';
-import legal from './legal.json';
-import modals from './modals.json';
-import onboarding from './onboarding.json';
-import operations from './operations.json';
-import password from './password.json';
-import postDetails from './postDetails.json';
-import postOperations from './postOperations.json';
-import profile from './profile.json';
-import relationships from './relationships.json';
-import search from './search.json';
-import selectProfile from './selectProfile.json';
-import settings from './settings.json';
-import signup from './signup.json';
-import sendTips from './tips.json';
-import tips from './tips.json';
-import permissions from './permissions.json';
+import activities from "./activities.json";
+import broadcastTx from "./broadcastTx.json";
+import common from "./common.json";
+import createPost from "./createPost.json";
+import createProfile from "./createProfile.json";
+import error from "./errors.json";
+import forgotPassword from "./forgotPassword.json";
+import home from "./home.json";
+import landing from "./landing.json";
+import legal from "./legal.json";
+import modals from "./modals.json";
+import onboarding from "./onboarding.json";
+import operations from "./operations.json";
+import password from "./password.json";
+import pictureEditor from "./pictureEditor.json";
+import postDetails from "./postDetails.json";
+import postOperations from "./postOperations.json";
+import profile from "./profile.json";
+import relationships from "./relationships.json";
+import search from "./search.json";
+import selectProfile from "./selectProfile.json";
+import settings from "./settings.json";
+import signup from "./signup.json";
+import sendTips from "./tips.json";
+import tips from "./tips.json";
+import permissions from "./permissions.json";
 
 const en = {
   activities,
@@ -40,6 +41,7 @@ const en = {
   operations,
   password,
   permissions,
+  pictureEditor,
   postDetails,
   postOperations,
   profile,
