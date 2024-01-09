6862c6fbf2a680b5151d1ac416b9c1bed2b553b8
Manuel <manuel.turetta94@gmail.com>
2024-01-09 15:31:39 +0100

    feat: update the usePost hook to update the local posts cache

--- src/hooks/posts/usePost.ts
+++ src/hooks/posts/usePost.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@apollo/client';
 import { useActiveAccountAddress } from '@recoil/accounts';
+import { useCachedPostById, useStorePost } from '@recoil/posts';
 import { convertGraphQLPost } from 'lib/GraphQLUtils';
-import { isEqual } from 'lodash';
-import { useMemo } from 'react';
+import React from 'react';
 import GetPostByID from 'services/graphql/queries/GetPostByID';
 import { Post } from 'types/posts';
 
@@ -15,30 +15,41 @@ const usePost = (postId: number, storedPost?: Post) => {
     throw new Error('Trying to get the details of a post without an active address');
   }
 
+  const post = useCachedPostById(activeAddress, postId);
+  const storePost = useStorePost();
+
+  // Callback to update the cached post after has been fetched.
+  const onDataFethed = React.useCallback(
+    async (data: any) => {
+      const fetchedPost = data?.posts.at(0);
+      if (fetchedPost) {
+        storePost(activeAddress, convertGraphQLPost(fetchedPost));
+      }
+    },
+    [activeAddress, storePost],
+  );
+
   // Query the post from the GraphQL server
-  const { data, refetch, loading } = useQuery(GetPostByID, {
+  const { refetch, loading } = useQuery(GetPostByID, {
     refetchWritePolicy: 'overwrite',
+    onCompleted: onDataFethed,
     variables: {
       postId,
     },
   });
 
-  /**
-   * Update the post state with the new post whenever the data changes
-   */
-  const post: Post | undefined = useMemo(() => {
-    if (!data || !data?.posts || !data?.posts[0]) {
-      return storedPost;
+  const cachedRefetch = React.useCallback(async () => {
+    const apolloResult = await refetch();
+    if (apolloResult.error === undefined) {
+      onDataFethed(apolloResult.data);
     }
-    const fetchedPost = data.posts[0];
-    const convertedPost = fetchedPost ? convertGraphQLPost(fetchedPost) : undefined;
-    return isEqual(fetchedPost, convertedPost) ? fetchedPost : convertedPost;
-  }, [data, storedPost]);
+    return apolloResult;
+  }, [onDataFethed, refetch]);
 
   return {
     loading,
-    post,
-    refetch,
+    post: post ?? storedPost,
+    refetch: cachedRefetch,
   };
 };
 
