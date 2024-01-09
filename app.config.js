21a97f5cafbe303ee608b5daa314941b161560c1
Manuel <manuel.turetta94@gmail.com>
2024-01-09 10:02:28 +0100

    chore: bump version to test the deploy

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'Mooncake',
     slug: 'mooncake',
     owner: 'desmos',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
