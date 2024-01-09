30a840b216ab720459684def9f965a1f5bf57092
Manuel <manuel.turetta94@gmail.com>
2024-01-09 11:46:45 +0100

    chore: revert version number bump

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'Mooncake',
     slug: 'mooncake',
     owner: 'desmos',
-    version: '1.0.1',
+    version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
