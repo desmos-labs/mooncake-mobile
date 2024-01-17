abaec7e819c104e6020f8116250d5208ac9b8064
Manuel <manuel.turetta94@gmail.com>
2024-01-17 16:19:36 +0100

    feat: handle failed fee grant request

--- src/screens/Onboarding/index.tsx
+++ src/screens/Onboarding/index.tsx
@@ -24,6 +24,11 @@ import GetFeeGrant from 'services/axios/requests/GetFeeGrant';
 import { AccountWithWallet } from 'types/account';
 import { DesmosProfile } from 'types/desmos';
 import { LoginOnboardingStep } from 'types/tourguide';
+import useTrackOnboardingCompleted from 'hooks/analytics/useTrackOnboardingCompleted';
+import { promiseToResult } from 'lib/NeverThrowUtils';
+import { err, ok } from 'neverthrow';
+import useErrorModal from 'hooks/modals/useErrorModal';
+import * as Sentry from 'sentry-expo';
 import useStyles, { fixedWidth } from './useStyles';
 
 export interface OnboardingParams {
@@ -66,7 +71,8 @@ const Onboarding = () => {
   // --- Local state
   // -------------------------------------------------------------------------------------
 
-  const [loading, setLoading] = useState(false);
+  const [hasFeeGrant, setHasFeeGrant] = useState(requestFeeGrant !== true);
+  const [feeGrantRequestFailed, setFeeGrantRequestFailed] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const slidesRef = useRef<FlatList>(null);
   const scrollX = useRef(new Animated.Value(0)).current;
@@ -107,16 +113,63 @@ const Onboarding = () => {
   const getAuthorizationInformation = useGetLazyAuthorizationInformation();
   const setTourGuideStep = useSetTourGuideStep();
   const trackOnboardingCompleted = useTrackOnboardingCompleted();
+  const showErrorModal = useErrorModal();
 
   // -------------------------------------------------------------------------------------
   // --- Actions
   // -------------------------------------------------------------------------------------
 
+  /**
+   * Sign up the user and request the fee grant
+   */
+  const signUp = useCallback(async () => {
+    if (account) {
+      const getAuthorizationResult = await promiseToResult(
+        getAuthorizationInformation(account.wallet.address),
+        'Unknown error while getting authorization information',
+      );
+      if (getAuthorizationResult.isErr()) {
+        return err(getAuthorizationResult.error);
+      }
+      const { feeGrants } = getAuthorizationResult.value;
+      const saveProfileAllowance = getSaveProfileAllowance(feeGrants);
+      // If has the save profile allowance
+      if (saveProfileAllowance === undefined) {
+        __DEV__ && console.log('[SignUp] Logged in and requested fee grant');
+        const getFeegranteResult = await GetFeeGrant();
+        if (getFeegranteResult.isErr()) {
+          return err(getFeegranteResult.error);
+        }
+      } else {
+        __DEV__ && console.log('[SignUp] Logged in, fee grant already requested and granted');
+      }
+    }
+    return ok(undefined);
+  }, [account, getAuthorizationInformation]);
+
+  const requestGrant = useCallback(async () => {
+    setHasFeeGrant(false);
+    setFeeGrantRequestFailed(false);
+    const signUpResult = await signUp();
+    if (signUpResult.isErr()) {
+      setFeeGrantRequestFailed(true);
+      Sentry.Native.captureException(signUpResult.error);
+      showErrorModal(
+        t('an error occured while requesting the fee grant', {
+          error: signUpResult.error.message,
+        }),
+        requestGrant,
+      );
+    } else {
+      setHasFeeGrant(true);
+    }
+  }, [showErrorModal, signUp, t]);
+
   const onPressButton = useCallback(() => {
     if (currentIndex < 3) {
       slidesRef?.current?.scrollToIndex({ index: currentIndex + 1 });
     } else {
-      if (!loading) {
+      if (hasFeeGrant) {
         trackOnboardingCompleted();
         setTourGuideStep({ login: LoginOnboardingStep.Completed });
         navigate(ROUTES.PASSWORD_MANIPULATION, {
@@ -124,35 +177,22 @@ const Onboarding = () => {
           account: params.account,
           profile: params.profile,
         });
+      } else if (feeGrantRequestFailed) {
+        requestGrant();
       }
     }
   }, [
     currentIndex,
-    loading,
+    feeGrantRequestFailed,
+    hasFeeGrant,
     navigate,
     params.account,
     params.passwordManipulationMode,
     params.profile,
+    requestGrant,
     setTourGuideStep,
     trackOnboardingCompleted,
   ]);
-
-  /**
-   * Sign up the user and request the fee grant
-   */
-  const signUp = useCallback(async () => {
-    if (account) {
-      const { feeGrants } = await getAuthorizationInformation(account.wallet.address);
-      const saveProfileAllowance = getSaveProfileAllowance(feeGrants);
-      // If has the save profile allowance
-      if (saveProfileAllowance === undefined) {
-        __DEV__ && console.log('[SignUp] Logged in and requested fee grant');
-        await GetFeeGrant();
-      } else {
-        __DEV__ && console.log('[SignUp] Logged in, fee grant already requested and granted');
-      }
-    }
-  }, [account, getAuthorizationInformation]);
 
   // -------------------------------------------------------------------------------------
   // --- Effects
@@ -160,12 +200,9 @@ const Onboarding = () => {
 
   useEffect(() => {
     if (requestFeeGrant) {
-      setLoading(true);
-      signUp().then(() => {
-        setLoading(false);
-      });
+      requestGrant();
     }
-  }, [account, params.requestFeeGrant, requestFeeGrant, signUp]);
+  }, [requestFeeGrant, requestGrant]);
 
   // -------------------------------------------------------------------------------------
   // --- Screen rendering
@@ -222,7 +259,7 @@ const Onboarding = () => {
           textColor={theme.colors.white}
           onPress={onPressButton}
           style={styles.button}>
-          {t('next', { ns: 'common' })}
+          {feeGrantRequestFailed ? t('request grant') : t('next', { ns: 'common' })}
         </Button>
         <Spacer paddingBottom="xl" />
       </View>
