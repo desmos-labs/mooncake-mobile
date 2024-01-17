cd09db224ff8d509abe1a6792002963a3b960bdf
Manuel <manuel.turetta94@gmail.com>
2024-01-17 12:33:43 +0100

    fix: return to setting screen

--- src/screens/PasswordManipulation/useHooks.ts
+++ src/screens/PasswordManipulation/useHooks.ts
@@ -259,10 +259,7 @@ const useHooks = () => {
             title: t('success', { ns: 'common' }),
             subtitle: t('you changed your password'),
             primaryButtonLabel: t('go back'),
-            onPressPrimary: () =>
-              navigate(ROUTES.BOTTOM_TABS, {
-                screen: ROUTES.SETTINGS,
-              }),
+            onPressPrimary: () => navigate(ROUTES.SETTINGS),
           });
         }
       }
