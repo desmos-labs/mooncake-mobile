bef890d6866d87542983c8fecba0cf3db05463bc
Manuel <manuel.turetta94@gmail.com>
2024-01-26 10:05:48 +0100

    feat: add keplr wallet definition

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -3,6 +3,7 @@ import { Signer } from '@desmoslabs/desmjs';
 export enum WalletSerializationVersion {
   Web3Auth = 1,
   PrivateKey = 1,
+  WallletConnectKeplr = 1,
 }
 
 /**
@@ -17,12 +18,20 @@ export enum WalletType {
    * Wallet that has been imported with a private key.
    */
   PrivateKey = 'private_key',
+  /**
+   * Wallet imported using WalletConnect.
+   */
+  WalletConnect = 'walletconnect',
 }
 
 /**
  * Interface holding the common fields between each wallet type.
  */
-interface BaseWallet {
+interface BaseWallet<T extends WalletType> {
+  /**
+   * The wallet type.
+   */
+  readonly type: T;
   /**
    * Bech32 Address prefix of this wallet.
    */
@@ -40,7 +49,7 @@ interface BaseWallet {
 /**
  * Interface that represents a generic wallet with a private key.
  */
-export interface WalletWithPrivateKey extends BaseWallet {
+export interface WalletWithPrivateKey<T extends WalletType> extends BaseWallet<T> {
   /**
    * Secp256k1 private key obtained from Web3Auth.
    */
@@ -50,8 +59,7 @@ export interface WalletWithPrivateKey extends BaseWallet {
 /**
  * Interface representing a wallet imported through Web3Auth.
  */
-export interface Web3AuthWallet extends WalletWithPrivateKey {
-  readonly type: WalletType.Web3Auth;
+export interface Web3AuthWallet extends WalletWithPrivateKey<WalletType.Web3Auth> {
   /**
    * Login method used from the user.
    */
@@ -72,9 +80,7 @@ export type SerializableWeb3AuthWallet = Omit<Web3AuthWallet, 'signer' | 'privat
 /**
  * Interface representing a wallet imported through a private key.
  */
-export interface PrivateKeyWallet extends WalletWithPrivateKey {
-  readonly type: WalletType.PrivateKey;
-}
+export interface PrivateKeyWallet extends WalletWithPrivateKey<WalletType.PrivateKey> {}
 
 /**
  * [PrivateKeyWallet] that can be serialized to JSON.
@@ -88,12 +94,71 @@ export type SerializablePrivateKeyWallet = Omit<PrivateKeyWallet, 'signer' | 'pr
 };
 
 /**
+ * The WalletConnect clients that the application supports.
+ */
+export enum WalletConnectWalletClient {
+  Keplr = 'keplr',
+}
+
+/**
+ * Interface that represents a wallet imported using WalletConnect.
+ */
+interface BaseWalletConnectWallet<C extends WalletConnectWalletClient>
+  extends BaseWallet<WalletType.WalletConnect> {
+  /**
+   * The client that the wallet is connected to.
+   */
+  readonly client: C;
+}
+
+/**
+ * Interface that represents a wallet imported using WalletConnect
+ * that can be serialization to JSON.
+ * @param C - The client that the wallet is connected to.
+ * @param V - The serialization version.
+ */
+interface BaseSerializableWalletConnectWallet<
+  C extends WalletConnectWalletClient,
+  V extends WalletSerializationVersion,
+> extends Omit<BaseWalletConnectWallet<C>, 'signer'> {
+  readonly version: V;
+}
+
+/**
+ * Interface that represents a wallet imported through Keplr.
+ */
+export interface KeplrWalletConnectWallet
+  extends BaseWalletConnectWallet<WalletConnectWalletClient.Keplr> {}
+
+/**
+ * [KeplrWalletConnectWallet] that can be serialized to JSON.
+ */
+export interface SerializableKeplrWalletConnectWallet
+  extends BaseSerializableWalletConnectWallet<
+    WalletConnectWalletClient.Keplr,
+    WalletSerializationVersion.WallletConnectKeplr
+  > {}
+
+/**
+ * Type representing a wallet that was imported through WalletConnect.
+ */
+export type WalletConnectWallet = KeplrWalletConnectWallet;
+
+/**
+ * Type representing a [WalletConnectWallet] that can be serialized to JSON.
+ */
+export type SerializableWalletConnectWallet = SerializableKeplrWalletConnectWallet;
+
+/**
  * Type representing all the supported wallets.
  */
-export type Wallet = Web3AuthWallet | PrivateKeyWallet;
+export type Wallet = Web3AuthWallet | PrivateKeyWallet | WalletConnectWallet;
 
 /**
  * Type representing a wallet that can be serialized to JSON and
  * stored in the device storage.
  */
-export type SerializableWallet = SerializableWeb3AuthWallet | SerializablePrivateKeyWallet;
+export type SerializableWallet =
+  | SerializableWeb3AuthWallet
+  | SerializablePrivateKeyWallet
+  | SerializableWalletConnectWallet;
