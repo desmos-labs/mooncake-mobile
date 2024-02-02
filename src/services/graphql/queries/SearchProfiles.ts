2a36e553cc41b22c85987137ab9385ca75ac1065
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-02-02 22:14:09 +0100

    fix: update api name inside GraphQL queries

--- src/services/graphql/queries/SearchProfiles.ts
+++ src/services/graphql/queries/SearchProfiles.ts
@@ -3,7 +3,7 @@ import ProfileFields from 'services/graphql/queries/fragments/ProfilesFields';
 
 const SearchProfiles = gql`
   ${ProfileFields}
-  query SearchProfiles($search: String, $offset: Int!, $limit: Int!) @api(name: desmos) {
+  query SearchProfiles($search: String, $offset: Int!, $limit: Int!) @api(name: butter) {
     profiles: profile(
       where: { _or: [{ dtag: { _ilike: $search } }, { nickname: { _ilike: $search } }] }
       offset: $offset
