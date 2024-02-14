b7afa15995abca061c129b0c2255c4e5112ae85a
Alessandro Mazzon <alessandro@forbole.com>
2024-02-14 15:39:33 +0100

    chore: release 1.4.5

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'Mooncake',
     slug: 'mooncake',
     owner: 'desmos',
-    version: '1.4.4',
+    version: '1.4.5',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
