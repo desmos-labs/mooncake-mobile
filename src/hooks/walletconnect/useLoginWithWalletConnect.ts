17254c522a9e7d7df989bb81e793297851e0cf59
Manuel <manuel.turetta94@gmail.com>
2024-02-14 11:53:21 +0100

    refactor: moved logic to generate the authorization messages

--- src/hooks/walletconnect/useLoginWithWalletConnect.ts
+++ src/hooks/walletconnect/useLoginWithWalletConnect.ts
@@ -1,70 +1,23 @@
 import { useCallback } from 'react';
 import { WalletConnectWalletApp } from 'types/wallet';
-import { initWalletConnectSession } from 'lib/WalletConnect';
+import {
+  MooncakePermissionMessages,
+  getWalletConnectPermissionMessages,
+  initWalletConnectSession,
+} from 'lib/WalletConnect';
 import useRootNavigator from 'hooks/navigation/useRootNavigator';
 import ROUTES from 'navigation/routes';
 import { useTranslation } from 'react-i18next';
 import { useCurrentChainGasPrice, useCurrentChainInfo } from '@recoil/settings';
 import { buildDesmosClient } from 'lib/TxUtils';
-import {
-  Authz,
-  EncodeObject,
-  Feegrant,
-  Posts,
-  PrivateKeySigner,
-  Profiles,
-  Reactions,
-  Relationships,
-  Reports,
-  Signer,
-  SigningMode,
-  TxRaw,
-} from '@desmoslabs/desmjs';
+import { PrivateKeySigner, Signer, SigningMode, TxRaw } from '@desmoslabs/desmjs';
 import { err, ok } from 'neverthrow';
-import { MsgGrant } from '@desmoslabs/desmjs-types/cosmos/authz/v1beta1/tx';
-import { MsgGrantAllowance } from 'cosmjs-types/cosmos/feegrant/v1beta1/tx';
-import { BasicAllowance, AllowedMsgAllowance } from 'cosmjs-types/cosmos/feegrant/v1beta1/feegrant';
-import { GenericAuthorization } from '@desmoslabs/desmjs-types/cosmos/authz/v1beta1/authz';
 import { promiseToResult } from 'lib/NeverThrowUtils';
 import useErrorModal from 'hooks/modals/useErrorModal';
 import { generateWalletConnectWallet } from 'lib/WalletUtils';
 import useSaveAccountAndCreateProfileFlow from 'hooks/accounts/useSaveAccountAndCreateProfile';
 import { AccountWithWallet } from 'types/account';
 import useConnectWalletConnect from './useConnectWalletConnect';
-
-const MooncakeMessages = [
-  // x/posts messages.
-  Posts.v3.MsgCreatePostTypeUrl,
-  Posts.v3.MsgEditPostTypeUrl,
-  Posts.v3.MsgDeletePostTypeUrl,
-  Posts.v3.MsgAddPostAttachmentTypeUrl,
-  Posts.v3.MsgRemovePostAttachmentTypeUrl,
-  Posts.v3.MsgAnswerPollTypeUrl,
-  Posts.v3.MsgRequestPostOwnerTransferTypeUrl,
-  Posts.v3.MsgCancelPostOwnerTransferRequestTypeUrl,
-  Posts.v3.MsgAcceptPostOwnerTransferRequestTypeUrl,
-  Posts.v3.MsgRefusePostOwnerTransferRequestTypeUrl,
-  // x/profiles message.
-  Profiles.v3.MsgSaveProfileTypeUrl,
-  Profiles.v3.MsgDeleteProfileTypeUrl,
-  Profiles.v3.MsgRequestDTagTransferTypeUrl,
-  Profiles.v3.MsgCancelDTagTransferRequestTypeUrl,
-  Profiles.v3.MsgAcceptDTagTransferRequestTypeUrl,
-  Profiles.v3.MsgRefuseDTagTransferRequestTypeUrl,
-  // x/reactions messages.
-  Reactions.v1.MsgAddReactionTypeUrl,
-  Reactions.v1.MsgRemoveReactionTypeUrl,
-  // x/relationships messages.
-  Relationships.v1.MsgCreateRelationshipTypeUrl,
-  Relationships.v1.MsgDeleteRelationshipTypeUrl,
-  Relationships.v1.MsgBlockUserTypeUrl,
-  Relationships.v1.MsgUnblockUserTypeUrl,
-  // x/reports
-  Reports.v1.MsgCreateReportTypeUrl,
-  Reports.v1.MsgDeleteReportTypeUrl,
-  // x/wasm messages.
-  '/cosmwasm.wasm.v1.MsgExecuteContract',
-];
 
 const usePromptGrantsRequest = () => {
   const navigation = useRootNavigator();
@@ -95,7 +48,14 @@ const useSignAndBroadcastGrants = () => {
 
   return useCallback(
     async (signer: Signer, granter: string, grantee: string) => {
-      const buildResult = await buildDesmosClient(chainInfo!.rpcUrl, signer, gasPrice);
+      if (chainInfo === undefined) {
+        return err(new Error('Unknwon error, chain info is undefined'));
+      }
+      if (gasPrice === undefined) {
+        return err(new Error('Unknwon error, gas price is undefined'));
+      }
+
+      const buildResult = await buildDesmosClient(chainInfo.rpcUrl, signer, gasPrice);
       if (buildResult.isErr()) {
         return err(buildResult.error);
       }
@@ -105,53 +65,7 @@ const useSignAndBroadcastGrants = () => {
       const expirationDate = new Date();
       expirationDate.setFullYear(expirationDate.getFullYear() + 1);
 
-      const messages: EncodeObject[] = MooncakeMessages.map(typeUrl => ({
-        typeUrl: Authz.v1beta1.MsgGrantTypeUrl,
-        value: MsgGrant.fromPartial({
-          grantee,
-          granter,
-          grant: {
-            authorization: {
-              typeUrl: Authz.v1beta1.GenericAuthorizationTypeUrl,
-              value: GenericAuthorization.encode(
-                GenericAuthorization.fromPartial({
-                  msg: typeUrl,
-                }),
-              ).finish(),
-            },
-            expiration: {
-              seconds: Math.ceil(expirationDate.getTime() / 1000),
-            },
-          },
-        }),
-      }));
-
-      // Add a Feegrant so that the temporary wallet can broadcast
-      // the transactions using the user's wallet balance.
-      messages.push({
-        typeUrl: Feegrant.v1beta1.MsgGrantAllowanceTypeUrl,
-        value: MsgGrantAllowance.fromPartial({
-          granter,
-          grantee,
-          allowance: {
-            typeUrl: Feegrant.v1beta1.AllowedMsgAllowanceTypeUrl,
-            value: AllowedMsgAllowance.encode(
-              AllowedMsgAllowance.fromPartial({
-                allowance: {
-                  typeUrl: Feegrant.v1beta1.BasicAllowanceTypeUrl,
-                  value: BasicAllowance.encode(
-                    BasicAllowance.fromPartial({
-                      expiration: { seconds: Math.ceil(expirationDate.getTime() / 1000) },
-                    }),
-                  ).finish(),
-                },
-                allowedMessages: MooncakeMessages,
-              }),
-            ).finish(),
-          },
-        }),
-      });
-
+      const messages = getWalletConnectPermissionMessages(granter, grantee, expirationDate);
       // Sign the transaction with the grant.
       const signResult = await promiseToResult(
         client.signTx(granter, messages),
@@ -174,7 +88,7 @@ const useSignAndBroadcastGrants = () => {
 
       return ok({
         signature: signResult.value,
-        authorizedMessages: MooncakeMessages,
+        authorizedMessages: MooncakePermissionMessages,
         grantExpiration: expirationDate,
       });
     },
@@ -215,7 +129,6 @@ const useLoginWithWalletConnect = () => {
       const signer = sessionInitializationResult.value;
       const accounts = await signer.getAccounts();
       const [account] = accounts;
-      console.log('[useLoginWithWalletConnect] Logged in as: ', account);
 
       // The session is established, prompt the user to authorize
       // our generated wallet to sign the message on the user
@@ -236,6 +149,7 @@ const useLoginWithWalletConnect = () => {
 
         if (signResult.isErr()) {
           showErrorMessage(t('grant transaction failed', { message: signResult.error.message }));
+          signer.disconnect();
           return;
         }
         walletConnectAccount = await generateWalletConnectWallet(app, signer, {
@@ -249,7 +163,12 @@ const useLoginWithWalletConnect = () => {
         walletConnectAccount = await generateWalletConnectWallet(app, signer);
       }
 
-      startSaveAccountAndCreateProfileFlow({ account: walletConnectAccount });
+      const startLoginResult = await startSaveAccountAndCreateProfileFlow({
+        account: walletConnectAccount,
+      });
+      if (startLoginResult.isErr()) {
+        signer.disconnect();
+      }
     },
     [
       connectWalletConnectClient,
--- src/lib/WalletConnect/index.ts
+++ src/lib/WalletConnect/index.ts
@@ -3,9 +3,61 @@ import { SessionTypes } from '@walletconnect/types';
 import { WalletConnectWalletApp } from 'types/wallet';
 import { Result } from 'neverthrow';
 import { WalletConnectSigner } from '@desmoslabs/desmjs-walletconnect-v2';
+import {
+  Authz,
+  EncodeObject,
+  Feegrant,
+  Posts,
+  Profiles,
+  Reactions,
+  Relationships,
+  Reports,
+} from '@desmoslabs/desmjs';
+import { MsgGrant } from '@desmoslabs/desmjs-types/cosmos/authz/v1beta1/tx';
+import { GenericAuthorization } from '@desmoslabs/desmjs-types/cosmos/authz/v1beta1/authz';
+import { MsgGrantAllowance } from 'cosmjs-types/cosmos/feegrant/v1beta1/tx';
+import { AllowedMsgAllowance, BasicAllowance } from 'cosmjs-types/cosmos/feegrant/v1beta1/feegrant';
 import { initDPMWalletConnectSession } from './dpm';
 
-// eslint-disable-next-line import/prefer-default-export
+/**
+ * List of messages that the application should
+ * be able to sign and broadcast in order to work
+ * when operates through an account on behalf of another user.
+ */
+export const MooncakePermissionMessages = [
+  // x/posts messages.
+  Posts.v3.MsgCreatePostTypeUrl,
+  Posts.v3.MsgEditPostTypeUrl,
+  Posts.v3.MsgDeletePostTypeUrl,
+  Posts.v3.MsgAddPostAttachmentTypeUrl,
+  Posts.v3.MsgRemovePostAttachmentTypeUrl,
+  Posts.v3.MsgAnswerPollTypeUrl,
+  Posts.v3.MsgRequestPostOwnerTransferTypeUrl,
+  Posts.v3.MsgCancelPostOwnerTransferRequestTypeUrl,
+  Posts.v3.MsgAcceptPostOwnerTransferRequestTypeUrl,
+  Posts.v3.MsgRefusePostOwnerTransferRequestTypeUrl,
+  // x/profiles message.
+  Profiles.v3.MsgSaveProfileTypeUrl,
+  Profiles.v3.MsgDeleteProfileTypeUrl,
+  Profiles.v3.MsgRequestDTagTransferTypeUrl,
+  Profiles.v3.MsgCancelDTagTransferRequestTypeUrl,
+  Profiles.v3.MsgAcceptDTagTransferRequestTypeUrl,
+  Profiles.v3.MsgRefuseDTagTransferRequestTypeUrl,
+  // x/reactions messages.
+  Reactions.v1.MsgAddReactionTypeUrl,
+  Reactions.v1.MsgRemoveReactionTypeUrl,
+  // x/relationships messages.
+  Relationships.v1.MsgCreateRelationshipTypeUrl,
+  Relationships.v1.MsgDeleteRelationshipTypeUrl,
+  Relationships.v1.MsgBlockUserTypeUrl,
+  Relationships.v1.MsgUnblockUserTypeUrl,
+  // x/reports
+  Reports.v1.MsgCreateReportTypeUrl,
+  Reports.v1.MsgDeleteReportTypeUrl,
+  // x/wasm messages.
+  '/cosmwasm.wasm.v1.MsgExecuteContract',
+];
+
 export const initWalletConnectSession = async (
   client: SignClient,
   app: WalletConnectWalletApp,
@@ -17,4 +69,67 @@ export const initWalletConnectSession = async (
     default:
       throw new Error(`unsupported WalletConnect app ${app}`);
   }
+};
+
+/**
+ * Function that provides the list of message that should be broadcasted
+ * in order to authorized the grantee to sign the transactions
+ * on behalf of the granter;
+ * @param granter - The address of the granter.
+ * @param grantee - The address of the grantee.
+ * @param expiration - The permissions expiration date.
+ */
+export const getWalletConnectPermissionMessages = (
+  granter: string,
+  grantee: string,
+  expiration: Date,
+): EncodeObject[] => {
+  const messages: EncodeObject[] = MooncakePermissionMessages.map(typeUrl => ({
+    typeUrl: Authz.v1beta1.MsgGrantTypeUrl,
+    value: MsgGrant.fromPartial({
+      grantee,
+      granter,
+      grant: {
+        authorization: {
+          typeUrl: Authz.v1beta1.GenericAuthorizationTypeUrl,
+          value: GenericAuthorization.encode(
+            GenericAuthorization.fromPartial({
+              msg: typeUrl,
+            }),
+          ).finish(),
+        },
+        expiration: {
+          seconds: Math.ceil(expiration.getTime() / 1000),
+        },
+      },
+    }),
+  }));
+
+  // Add a Feegrant so that the temporary wallet can broadcast
+  // the transactions using the user's wallet balance.
+  messages.push({
+    typeUrl: Feegrant.v1beta1.MsgGrantAllowanceTypeUrl,
+    value: MsgGrantAllowance.fromPartial({
+      granter,
+      grantee,
+      allowance: {
+        typeUrl: Feegrant.v1beta1.AllowedMsgAllowanceTypeUrl,
+        value: AllowedMsgAllowance.encode(
+          AllowedMsgAllowance.fromPartial({
+            allowance: {
+              typeUrl: Feegrant.v1beta1.BasicAllowanceTypeUrl,
+              value: BasicAllowance.encode(
+                BasicAllowance.fromPartial({
+                  expiration: { seconds: Math.ceil(expiration.getTime() / 1000) },
+                }),
+              ).finish(),
+            },
+            allowedMessages: MooncakePermissionMessages,
+          }),
+        ).finish(),
+      },
+    }),
+  });
+
+  return messages;
 };
