132d3527f985b6668fc193917a695624f7185217
Alessandro Mazzon <alexmazzon@gmail.com>
2024-02-27 10:36:24 +0100

    fix: lint

--- src/hooks/transactions/usePastTransactions.ts
+++ src/hooks/transactions/usePastTransactions.ts
@@ -1,44 +1,9 @@
-import { useApolloClient } from '@apollo/client';
 import { useUserPendingTransactions } from '@recoil/transactions';
-import { FetchDataFunction } from 'hooks/usePaginatedData';
 import usePaginatedQuery from 'hooks/usePaginatedQuery';
 import { convertGraphQLTransactionMessage } from 'lib/GraphQLUtils/transactions';
 import React, { useCallback, useMemo } from 'react';
 import GetTransactionsByAddress from 'services/graphql/queries/GetTransactionsByAddress';
 import { PastTransactionMessage, PendingTransaction } from 'types/transactions';
-
-const useFetchPastTransactions = (userAddress: string) => {
-  // Here we use the useApolloClient hook instead of the useLazyQuery hook because
-  // that hook dont behaves correctly if in the future we may want to implement
-  // the possibility to switch between mainnet and testnet.
-  const apollo = useApolloClient();
-
-  return React.useCallback<FetchDataFunction<PastTransactionMessage>>(
-    async (offset, limit) => {
-      const { data, error } = await apollo.query({
-        query: GetTransactionsByAddress,
-        variables: {
-          address: `{${userAddress}}`,
-          types: '{}',
-          limit,
-          offset,
-        },
-      });
-
-      if (error) {
-        throw error;
-      }
-
-      const fetchedData = data?.messages ?? [];
-      const converted = fetchedData.map(convertGraphQLTransactionMessage);
-      return {
-        data: converted,
-        endReached: fetchedData.length < limit,
-      };
-    },
-    [apollo, userAddress],
-  );
-};
 
 /**
  * Function that converts a {@link PendingTransaction} into a list of {@link PastTransactionMessage}
