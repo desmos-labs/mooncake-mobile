566cd9a554619389c1ec89dce1eaaa359d9c22a7
Manuel <manuel.turetta94@gmail.com>
2024-03-06 17:30:23 +0100

    feat: allow to retry a login with WalletConnect on failure

--- src/hooks/walletconnect/useLoginWithWalletConnect.ts
+++ src/hooks/walletconnect/useLoginWithWalletConnect.ts
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { WalletConnectWalletApp } from 'types/wallet';
 import {
   MooncakePermissionMessages,
@@ -10,13 +10,20 @@ import ROUTES from 'navigation/routes';
 import { useTranslation } from 'react-i18next';
 import { useCurrentChainGasPrice, useCurrentChainInfo } from '@recoil/settings';
 import { buildDesmosClient } from 'lib/TxUtils';
-import { PrivateKeySigner, Signer, SigningMode, TxRaw } from '@desmoslabs/desmjs';
+import { PrivateKeySigner, SignatureResult, Signer, SigningMode, TxRaw } from '@desmoslabs/desmjs';
 import { Result, err, ok } from 'neverthrow';
 import { promiseToResult } from 'lib/NeverThrowUtils';
 import { generateWalletConnectWallet } from 'lib/WalletUtils';
 import useSaveAccountAndCreateProfileFlow from 'hooks/accounts/useSaveAccountAndCreateProfile';
 import { AccountWithWallet } from 'types/account';
+import { WalletConnectSigner } from '@desmoslabs/desmjs-walletconnect-v2';
 import useConnectWalletConnect from './useConnectWalletConnect';
+
+interface GrantsSignResult {
+  readonly signature: SignatureResult;
+  readonly authorizedMessages: string[];
+  readonly grantExpiration: Date;
+}
 
 const usePromptGrantsRequest = () => {
   const navigation = useRootNavigator();
@@ -104,65 +111,103 @@ const useLoginWithWalletConnect = () => {
   const promptGrantsRequest = usePromptGrantsRequest();
   const signAndBroadcastGrants = useSignAndBroadcastGrants();
   const startSaveAccountAndCreateProfileFlow = useSaveAccountAndCreateProfileFlow();
+  // Cached WalletConnectSigner that has been previously authorized
+  // by the user.
+  const cachedWalletConnectSigner = useRef<WalletConnectSigner>();
+  // Reference to a previously generated signer,
+  // in case we need to retry the session initialization.
+  const generatedSigner = useRef<PrivateKeySigner>();
+  // Cached grants transaction signature result.
+  // If this is defined means that the user has already
+  // broadcasted a transaction that gives the permissions
+  // to our generated signer.
+  const grantsSignResult = useRef<GrantsSignResult>();
 
   return useCallback(
     async (app: WalletConnectWalletApp): Promise<Result<void, Error>> => {
-      // Initialize the WalletConnect client.
-      const connectionResult = await connectWalletConnectClient();
-      if (connectionResult.isErr()) {
-        return err(connectionResult.error);
+      let walletConnectSigner = cachedWalletConnectSigner.current;
+
+      if (walletConnectSigner === undefined) {
+        // Initialize the WalletConnect client.
+        const connectionResult = await connectWalletConnectClient();
+        if (connectionResult.isErr()) {
+          return err(connectionResult.error);
+        }
+
+        // Start the session by sending the request to the external wallet.
+        const client = connectionResult.value;
+        const sessionInitializationResult = await initWalletConnectSession(client, app);
+        if (sessionInitializationResult.isErr()) {
+          return err(sessionInitializationResult.error);
+        }
+        walletConnectSigner = sessionInitializationResult.value;
+        cachedWalletConnectSigner.current = walletConnectSigner;
       }
 
-      // Start the session by sending the request to the external wallet.
-      const client = connectionResult.value;
-      const sessionInitializationResult = await initWalletConnectSession(client, app);
-      if (sessionInitializationResult.isErr()) {
-        return err(sessionInitializationResult.error);
-      }
-
-      // Session established, get the account.
-      const signer = sessionInitializationResult.value;
-      const accounts = await signer.getAccounts();
+      // We have a WalletConnect session, get the account.
+      const accounts = await walletConnectSigner.getAccounts();
       const [account] = accounts;
 
-      // The session is established, prompt the user to authorize
+      // The session is established, if the user didn't accepted
+      // a previous request prompt the user to authorize
       // our generated wallet to sign the message on the user
       // behalf.
-      const authorized = await promptGrantsRequest();
+      const authorized = generatedSigner.current !== undefined || (await promptGrantsRequest());
       let walletConnectAccount: AccountWithWallet;
       if (authorized) {
         // Generate a new private key signer that will be used by the
         // application to sign the transactions without the need to
         // open the external wallet.
-        const tempWalletSigner = PrivateKeySigner.generate(SigningMode.AMINO);
-        await tempWalletSigner.connect();
-        // Sign and broadcast the authorization messages.
-        const grantee = await tempWalletSigner
-          .getCurrentAccount()
-          .then(tempWalletAccount => tempWalletAccount!.address);
-        const signResult = await signAndBroadcastGrants(signer, account.address, grantee);
-
-        if (signResult.isErr()) {
-          signer.disconnect();
-          return err(signResult.error);
+        let tempWalletSigner = generatedSigner.current;
+        if (tempWalletSigner === undefined) {
+          tempWalletSigner = PrivateKeySigner.generate(SigningMode.AMINO);
+          // Cache the generated signer.
+          generatedSigner.current = tempWalletSigner;
         }
-        walletConnectAccount = await generateWalletConnectWallet(app, signer, {
+        await tempWalletSigner.connect();
+
+        if (grantsSignResult.current === undefined) {
+          // Sign and broadcast the authorization messages.
+          const grantee = await tempWalletSigner
+            .getCurrentAccount()
+            .then(tempWalletAccount => tempWalletAccount!.address);
+          const signResult = await signAndBroadcastGrants(
+            walletConnectSigner,
+            account.address,
+            grantee,
+          );
+
+          if (signResult.isErr()) {
+            return err(signResult.error);
+          }
+
+          grantsSignResult.current = signResult.value;
+        }
+
+        walletConnectAccount = await generateWalletConnectWallet(app, walletConnectSigner, {
           signer: tempWalletSigner,
-          authorizations: signResult.value.authorizedMessages,
-          authorizationsExpiration: signResult.value.grantExpiration,
+          authorizations: grantsSignResult.current.authorizedMessages,
+          authorizationsExpiration: grantsSignResult.current.grantExpiration,
         });
       } else {
         // We don't have the user authorization, lets create an account without
         // the temp wallet.
-        walletConnectAccount = await generateWalletConnectWallet(app, signer);
+        walletConnectAccount = await generateWalletConnectWallet(app, walletConnectSigner);
       }
 
       const startLoginResult = await startSaveAccountAndCreateProfileFlow({
         account: walletConnectAccount,
       });
+
       if (startLoginResult.isErr()) {
-        signer.disconnect();
+        return err(startLoginResult.error);
       }
+
+      // The login succeed, lets clear the ref to avoid the disconnection of our
+      // WalletConnect signer.
+      cachedWalletConnectSigner.current = undefined;
+      generatedSigner.current = undefined;
+      grantsSignResult.current = undefined;
       return ok(undefined);
     },
     [
